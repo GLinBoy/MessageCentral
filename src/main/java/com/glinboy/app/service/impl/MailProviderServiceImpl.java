324c3dd0916da5841a560b88cf7565f64f640aa6
GLinBoy <hojjatabedie@gmail.com>
2021-04-20 17:27:33 +0430

    👌 IMPROVE: Use Message Queue to send email
    resolved #16

--- src/main/java/com/glinboy/app/service/impl/MailProviderServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/MailProviderServiceImpl.java
@@ -1,7 +1,14 @@
 package com.glinboy.app.service.impl;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,22 +18,30 @@ import com.glinboy.app.service.MailProviderService;
 import com.glinboy.app.service.dto.EmailDTO;
 
 @Service
-public class MailProviderServiceImpl implements MailProviderService<EmailDTO> {
+public class MailProviderServiceImpl implements MailProviderService<EmailDTO>, MessageListener {
 
 	private final Logger log = LoggerFactory.getLogger(MailProviderServiceImpl.class);
 	
 	private final ApplicationProperties properties;
 	
 	private final JavaMailSender emailSender;
+	
+	private final JmsTemplate jmsTemplate;
 
 	public MailProviderServiceImpl(JavaMailSender emailSender,
+			JmsTemplate jmsTemplate,
 			ApplicationProperties properties) {
 		this.emailSender = emailSender;
+		this.jmsTemplate = jmsTemplate;
 		this.properties = properties;
 	}
 
 	@Override
 	public void sendEmail(EmailDTO emailDTO) {
+		jmsTemplate.convertAndSend("MAILBOX", emailDTO);
+	}
+	
+	private void deliverEmail(EmailDTO emailDTO) {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setFrom(properties.getEmail().getFrom());
 		message.setTo(emailDTO.getReceiver());
@@ -34,6 +49,18 @@ public class MailProviderServiceImpl implements MailProviderService<EmailDTO> {
 		message.setText(emailDTO.getContent());
 		emailSender.send(message);
 		log.info("Mail sended! {}", emailDTO);
+	}
+
+	@Override
+	@JmsListener(destination = "MAILBOX")
+	public void onMessage(Message message) {
+		try {
+			ObjectMessage objectMessage = (ObjectMessage)message;
+			EmailDTO emailDTO = (EmailDTO)objectMessage.getObject();
+			deliverEmail(emailDTO);
+		} catch (JMSException e) {
+			log.error("Parsing message failed.", e);
+		}
 	}
 
 }
