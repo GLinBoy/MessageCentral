9288c890581c2ecc7e04b090c60dc9b772b3e914
GLinBoy <hojjatabedie@gmail.com>
2021-04-24 17:24:43 +0430

    📦 NEW: Added Notification provider service
    #28

--- src/main/java/com/glinboy/app/service/NotificationProviderService.java
+++ src/main/java/com/glinboy/app/service/NotificationProviderService.java
@@ -1,5 +1,9 @@
 package com.glinboy.app.service;
 
-public interface NotificationProviderService {
+import com.glinboy.app.service.dto.NotificationDTO;
+
+public interface NotificationProviderService<T extends NotificationDTO> {
+
+	void sendNotification(T t);
 
 }
