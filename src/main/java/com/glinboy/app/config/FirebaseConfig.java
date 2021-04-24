9463f714918bac99d08789572a50d4868eee7a99
GLinBoy <hojjatabedie@gmail.com>
2021-04-24 18:44:03 +0430

    🐛 FIX: Broken test

--- src/main/java/com/glinboy/app/config/FirebaseConfig.java
+++ src/main/java/com/glinboy/app/config/FirebaseConfig.java
@@ -7,11 +7,13 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
 	@Value("${application.notification.firebase.file-path}")
