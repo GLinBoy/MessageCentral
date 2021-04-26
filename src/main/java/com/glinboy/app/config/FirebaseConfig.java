61947f625ccae09457ee9138e24f29b9753f7e58
GLinBoy <hojjatabedie@gmail.com>
2021-04-26 16:03:18 +0430

    👌 IMPROVE: Use @ConditionalOnProperty to load Firebase config file

--- src/main/java/com/glinboy/app/config/FirebaseConfig.java
+++ src/main/java/com/glinboy/app/config/FirebaseConfig.java
@@ -1,19 +1,20 @@
 package com.glinboy.app.config;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
 
 @Configuration
-@Profile("!test")
+@ConditionalOnProperty(value = "application.notification.provider", havingValue = "firebase")
 public class FirebaseConfig {
 
 	@Value("${application.notification.firebase.file-path}")
