ad9c2ac1e13b70d2a346a8b277e3bc974456f026
GLinBoy <hojjatabedie@gmail.com>
2021-05-03 21:15:24 +0430

    📦 NEW: Added DTO for multiple SMS
    #40

---
+++ src/main/java/com/glinboy/app/service/dto/ShortMessagesDTO.java
@@ -0,0 +1,73 @@
+package com.glinboy.app.service.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.apache.commons.collections.CollectionUtils;
+
+/**
+ * A DTO for the {@link com.glinboy.app.domain.ShortMessage} entity.
+ */
+public class ShortMessagesDTO implements Serializable {
+
+    @NotEmpty
+    private Set<String> phoneNumbers = new HashSet<>();
+
+    @NotNull
+    @Size(min = 6, max = 160)
+    private String content;
+
+    public Set<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumber(Set<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ShortMessagesDTO)) {
+            return false;
+        }
+
+        ShortMessagesDTO shortMessagesDTO = (ShortMessagesDTO) o;
+
+        return !CollectionUtils.isEmpty(this.phoneNumbers) &&
+                this.phoneNumbers.size() == shortMessagesDTO.getPhoneNumbers().size() &&
+                this.phoneNumbers.containsAll(shortMessagesDTO.phoneNumbers) &&
+                (this.content == null ? shortMessagesDTO.content == null : this.content.equals(shortMessagesDTO.content));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.phoneNumbers.stream().collect(Collectors.joining(", ")) + this.content);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "ShortMessageDTO{" +
+            ", phoneNumber='" + getPhoneNumbers().stream().collect(Collectors.joining(", ")) + "'" +
+            ", content='" + getContent() + "'" +
+            "}";
+    }
+}
