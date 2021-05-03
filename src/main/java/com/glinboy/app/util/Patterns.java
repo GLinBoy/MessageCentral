15b77bf4d83e36be3fb2aa1e0317e8d5a99f358c
GLinBoy <hojjatabedie@gmail.com>
2021-05-03 21:26:05 +0430

    📦 NEW: Added mobile number regex pattern
    #40

--- src/main/java/com/glinboy/app/util/Patterns.java
+++ src/main/java/com/glinboy/app/util/Patterns.java
@@ -5,5 +5,7 @@ public final class Patterns {
 	private Patterns() {}
 	
 	public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+	
+	public static final String SMS_PATTERN = "^\\+(?:[0-9]●?){6,14}[0-9]$";
 
 }
