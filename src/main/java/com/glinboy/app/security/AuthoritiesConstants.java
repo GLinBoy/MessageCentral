7e08b2d8fa4f45497b493dc6ed4e099bf4623605
GLinBoy <hojjatabedie@gmail.com>
2021-05-06 19:49:59 +0430

    📦 NEW: Added 3 new roles for manage access to API
    Source: https://www.jhipster.tech/tips/025_tip_create_new_authority.html

--- src/main/java/com/glinboy/app/security/AuthoritiesConstants.java
+++ src/main/java/com/glinboy/app/security/AuthoritiesConstants.java
@@ -11,5 +11,11 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String EMAIL_USER = "ROLE_EMAIL_USER";
+
+    public static final String SMS_USER = "ROLE_SMS_USER";
+
+    public static final String NOTIFICATION_USER = "ROLE_NOTIFICATION_USER";
+
     private AuthoritiesConstants() {}
 }
