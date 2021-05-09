57565f609dd3c105191dcdb8eb502d8940494ef4
GLinBoy <hojjatabedie@gmail.com>
2021-05-09 18:20:16 +0430

    👌 IMPROVE: Update footer template
    resolved #79

--- src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.component.ts
@@ -1,4 +1,66 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class JhiFooter extends Vue {}
+export default class JhiFooter extends Vue {
+  public jhipsterLinks = [
+    {
+      href: 'https://www.jhipster.tech/',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.homepage',
+      text: 'JHipster homepage',
+    },
+    {
+      href: 'http://stackoverflow.com/tags/jhipster/info',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.stackoverflow',
+      text: 'JHipster on Stack Overflow',
+    },
+    {
+      href: 'https://github.com/jhipster/generator-jhipster/issues?state=open',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.bugtracker',
+      text: 'JHipster bug tracker',
+    },
+    {
+      href: 'https://gitter.im/jhipster/generator-jhipster',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.chat',
+      text: 'JHipster public chat room',
+    },
+    {
+      href: 'https://twitter.com/jhipster',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.follow',
+      text: 'follow @jhipster on Twitter',
+    },
+  ];
+
+  public mcLinks = [
+    {
+      href: 'https://github.com/GLinBoy/MessageCentral',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.homepage',
+      text: 'MessageCentral homepage',
+    },
+    {
+      href: 'https://github.com/GLinBoy/MessageCentral/issues?state=open',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.bugtracker',
+      text: 'MessageCentral bug tracker',
+    },
+    {
+      href: 'https://t.me/MessageCentral',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+      vtext: 'home.link.chat',
+      text: 'MessageCentral public chat room',
+    },
+  ];
+}
