b20c435b5b1685e7847b4fa10db9279cd47c76ae
GLinBoy <hojjatabedie@gmail.com>
2021-05-15 14:39:16 +0430

    👌 IMPROVE: Remove edit button from Email view page
    #107

--- src/main/webapp/app/entities/email/email-details.vue
+++ src/main/webapp/app/entities/email/email-details.vue
@@ -28,11 +28,6 @@
         <button type="submit" v-on:click.prevent="previousState()" class="btn btn-info" data-cy="entityDetailsBackButton">
           <font-awesome-icon icon="arrow-left"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.back')"> Back</span>
         </button>
-        <router-link v-if="email.id" :to="{ name: 'EmailEdit', params: { emailId: email.id } }" custom v-slot="{ navigate }">
-          <button @click="navigate" class="btn btn-primary">
-            <font-awesome-icon icon="pencil-alt"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.edit')"> Edit</span>
-          </button>
-        </router-link>
       </div>
     </div>
   </div>
