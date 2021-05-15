43fd30b34184ca20cc690cd5b5261afa0ad9b5b9
GLinBoy <hojjatabedie@gmail.com>
2021-05-15 14:48:24 +0430

    👌 IMPROVE: Remove edit button from Notification view page
    #109

--- src/main/webapp/app/entities/notification/notification-details.vue
+++ src/main/webapp/app/entities/notification/notification-details.vue
@@ -40,16 +40,6 @@
         <button type="submit" v-on:click.prevent="previousState()" class="btn btn-info" data-cy="entityDetailsBackButton">
           <font-awesome-icon icon="arrow-left"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.back')"> Back</span>
         </button>
-        <router-link
-          v-if="notification.id"
-          :to="{ name: 'NotificationEdit', params: { notificationId: notification.id } }"
-          custom
-          v-slot="{ navigate }"
-        >
-          <button @click="navigate" class="btn btn-primary">
-            <font-awesome-icon icon="pencil-alt"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.edit')"> Edit</span>
-          </button>
-        </router-link>
       </div>
     </div>
   </div>
