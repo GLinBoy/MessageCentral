184550241682a8aeb61b80fdcef662bcc8eea80c
GLinBoy <hojjatabedie@gmail.com>
2021-05-26 22:47:50 +0430

    👌 IMPROVE: Extend NotificationProviderService from ProviderService
    resolved #146

--- src/main/java/com/glinboy/app/service/NotificationProviderService.java
+++ src/main/java/com/glinboy/app/service/NotificationProviderService.java
@@ -1,13 +1,6 @@
 package com.glinboy.app.service;
 
-import java.util.List;
-
 import com.glinboy.app.service.dto.NotificationDTO;
 
-public interface NotificationProviderService<T extends NotificationDTO> {
-
-	void sendNotification(T t);
-
-	void sendNotification(List<NotificationDTO> dtoList);
-
+public interface NotificationProviderService<T extends NotificationDTO> extends ProviderService<T> {
 }
