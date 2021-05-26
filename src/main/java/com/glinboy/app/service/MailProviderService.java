ac8b67484b63c769b99745ad68ef88ddaf625a7e
GLinBoy <hojjatabedie@gmail.com>
2021-05-26 22:46:17 +0430

    👌 IMPROVE: Extended MailProviderService from ProviderService
    resolved #144

--- src/main/java/com/glinboy/app/service/MailProviderService.java
+++ src/main/java/com/glinboy/app/service/MailProviderService.java
@@ -1,13 +1,6 @@
 package com.glinboy.app.service;
 
-import java.util.List;
-
 import com.glinboy.app.service.dto.EmailDTO;
 
-public interface MailProviderService<T extends EmailDTO> {
-
-	void sendEmail(T t);
-
-	void sendEmail(List<T> dtoList);
-
+public interface MailProviderService<T extends EmailDTO> extends ProviderService<T> {
 }
