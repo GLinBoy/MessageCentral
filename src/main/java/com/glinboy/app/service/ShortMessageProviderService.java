b594e2623d46a9fc53c1325b828d05ea79ed4f55
GLinBoy <hojjatabedie@gmail.com>
2021-05-26 22:46:53 +0430

    👌 IMPROVE: Extended MailProviderService from ProviderService
    resolved #145

--- src/main/java/com/glinboy/app/service/ShortMessageProviderService.java
+++ src/main/java/com/glinboy/app/service/ShortMessageProviderService.java
@@ -1,12 +1,6 @@
 package com.glinboy.app.service;
 
-import java.util.List;
-
 import com.glinboy.app.service.dto.ShortMessageDTO;
 
-public interface ShortMessageProviderService<T extends ShortMessageDTO> {
-
-	void sendSMS(T t);
-	void sendSMS(List<T> t);
-
+public interface ShortMessageProviderService<T extends ShortMessageDTO> extends ProviderService<T> {
 }
