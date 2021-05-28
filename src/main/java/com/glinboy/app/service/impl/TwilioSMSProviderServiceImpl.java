01d4c709ed10a2e4d526c769f302ba15b16db747
GLinBoy <hojjatabedie@gmail.com>
2021-05-28 15:17:56 +0430

    👌 IMPROVE: Extend Twillio SMS Provider from GenericChannelServiceImpl
    resolved #154

--- src/main/java/com/glinboy/app/service/impl/TwilioSMSProviderServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/TwilioSMSProviderServiceImpl.java
@@ -1,14 +1,9 @@
 package com.glinboy.app.service.impl;
 
-import java.util.List;
-
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
@@ -22,47 +17,41 @@ import com.twilio.type.PhoneNumber;
 
 @Service
 @ConditionalOnProperty(value = "application.sms.provider", havingValue = "twilio", matchIfMissing = true)
-public class TwilioSMSProviderServiceImpl implements ShortMessageProviderService<ShortMessageDTO>, MessageListener {
+public class TwilioSMSProviderServiceImpl extends GenericChannelServiceImpl<ShortMessageDTO> implements ShortMessageProviderService<ShortMessageDTO> {
+	
+	public static final String TOPIC_NAME = "TWILIO_SMSBOX";
 
-	private final Logger log = LoggerFactory.getLogger(TwilioSMSProviderServiceImpl.class);
-
-	private final ApplicationProperties properties;
-
-	private final JmsTemplate jmsTemplate;
-
-	public TwilioSMSProviderServiceImpl(JmsTemplate jmsTemplate, ApplicationProperties properties) {
-		this.jmsTemplate = jmsTemplate;
-		this.properties = properties;
+	protected TwilioSMSProviderServiceImpl(JmsTemplate jmsTemplate,
+			ApplicationProperties properties) {
+		super(jmsTemplate, properties);
 	}
 
 	@Override
-	public void sendSMS(ShortMessageDTO shortMessageDTO) {
-		jmsTemplate.convertAndSend("TWILIO_SMSBOX", shortMessageDTO);
+	String getTopicName() {
+		return TOPIC_NAME;
 	}
 
 	@Override
-	public void sendSMS(List<ShortMessageDTO> shortMessagesDTO) {
-		shortMessagesDTO.forEach(s -> jmsTemplate.convertAndSend("TWILIO_SMSBOX", s));
-	}
-
-	private void deliverSMS(ShortMessageDTO shortMessageDTO) {
+	public void deliverMessage(ShortMessageDTO... shortMessageDTOs) {
 		Twilio.init(properties.getCredential().getTwilio().getAccountSid(),
 				properties.getCredential().getTwilio().getToken());
-		com.twilio.rest.api.v2010.account.Message message = com.twilio.rest.api.v2010.account.Message
-				.creator(new PhoneNumber(shortMessageDTO.getPhoneNumber()),
-						new PhoneNumber(properties.getSms().getFrom()), shortMessageDTO.getContent())
-				.create();
-		log.info("SMS sent! {}", shortMessageDTO);
-		log.info("SMS Result {}", message);
+		for (var i = 0; i < shortMessageDTOs.length; i++) {
+			var message = com.twilio.rest.api.v2010.account.Message
+					.creator(new PhoneNumber(shortMessageDTOs[i].getPhoneNumber()),
+							new PhoneNumber(properties.getSms().getFrom()), shortMessageDTOs[i].getContent())
+					.create();
+			log.info("SMS sent! {}", shortMessageDTOs[i]);
+			log.info("SMS Result {}", message);
+		}
 	}
 
 	@Override
-	@JmsListener(destination = "TWILIO_SMSBOX")
+	@JmsListener(destination = TwilioSMSProviderServiceImpl.TOPIC_NAME)
 	public void onMessage(Message message) {
 		try {
-			ObjectMessage objectMessage = (ObjectMessage) message;
-			ShortMessageDTO shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
-			deliverSMS(shortMessageDTO);
+			var objectMessage = (ObjectMessage) message;
+			var shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
+			this.deliverMessage(shortMessageDTO);
 		} catch (JMSException e) {
 			log.error("Parsing message failed: {}", e.getMessage(), e);
 		}
