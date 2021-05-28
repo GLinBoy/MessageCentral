8119647c4235e7f9649c803f0420b3816e4bbea2
GLinBoy <hojjatabedie@gmail.com>
2021-05-28 15:26:51 +0430

    👌 IMPROVE: Pushe using GenericChannelServiceImpl
    resolved #157

--- src/main/java/com/glinboy/app/service/impl/PusheNotificationProviderServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/PusheNotificationProviderServiceImpl.java
@@ -5,13 +5,10 @@ import java.util.List;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -27,61 +24,54 @@ import com.glinboy.app.service.dto.NotificationDTO;
 
 @Service
 @ConditionalOnProperty(value = "application.notification.provider", havingValue = "pushe")
-public class PusheNotificationProviderServiceImpl
-	implements NotificationProviderService<NotificationDTO>, MessageListener {
-
-	private final Logger log = LoggerFactory.getLogger(PusheNotificationProviderServiceImpl.class);
+public class PusheNotificationProviderServiceImpl extends GenericChannelServiceImpl<NotificationDTO>
+	implements NotificationProviderService<NotificationDTO> {
 	
-	private final ApplicationProperties properties;
+	public static final String TOPIC_NAME = "PUSHE_NOTIFICATIONBOX";
 
-	private final JmsTemplate jmsTemplate;
-
-	public PusheNotificationProviderServiceImpl(ApplicationProperties properties, JmsTemplate jmsTemplate) {
-		this.properties = properties;
-		this.jmsTemplate = jmsTemplate;
+	protected PusheNotificationProviderServiceImpl(JmsTemplate jmsTemplate,
+			ApplicationProperties properties) {
+		super(jmsTemplate, properties);
 	}
 
 	@Override
-	public void sendNotification(NotificationDTO notificationDTO) {
-		jmsTemplate.convertAndSend("PUSHE_NOTIFICATIONBOX", notificationDTO);
-		
+	String getTopicName() {
+		return TOPIC_NAME;
 	}
 
 	@Override
-	public void sendNotification(List<NotificationDTO> notificationDTOs) {
-		notificationDTOs.forEach(n -> jmsTemplate.convertAndSend("PUSHE_NOTIFICATIONBOX", n));
-	}
-	
-	private void deliverNotification(NotificationDTO notificationDTO) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.setAcceptCharset(List.of(StandardCharsets.UTF_8));
-		headers.set("authorization", "Token ".concat(properties.getNotification().getPushe().getToken()));
-		JSONObject requestBody = new JSONObject();
-		requestBody.put("app_ids", properties.getNotification().getPushe().getAppId());
-		JSONObject filters = new JSONObject();
-		filters.put("device_id", new JSONArray(List.of(notificationDTO.getToken())));
-		requestBody.put("filters", filters);
-		JSONObject data = new JSONObject();
-		data.put("title", notificationDTO.getSubject());
-		data.put("content", notificationDTO.getContent());
-		requestBody.put("data", data);
-		HttpEntity<String> request = 
-				new HttpEntity<>(requestBody.toString(), headers);
-		String result = restTemplate.postForObject(properties.getNotification().getPushe().getUrl(),
-				request, String.class);
-		log.info("Notification sent! {}", notificationDTO);
-		log.info("Notification Result {}", result);
+	public void deliverMessage(NotificationDTO... notificationDTOs) {
+		for (var i = 0; i < notificationDTOs.length; i++) {
+			var restTemplate = new RestTemplate();
+			var headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.setAcceptCharset(List.of(StandardCharsets.UTF_8));
+			headers.set("authorization", "Token ".concat(properties.getNotification().getPushe().getToken()));
+			var requestBody = new JSONObject();
+			requestBody.put("app_ids", properties.getNotification().getPushe().getAppId());
+			var filters = new JSONObject();
+			filters.put("device_id", new JSONArray(List.of(notificationDTOs[i].getToken())));
+			requestBody.put("filters", filters);
+			var data = new JSONObject();
+			data.put("title", notificationDTOs[i].getSubject());
+			data.put("content", notificationDTOs[i].getContent());
+			requestBody.put("data", data);
+			HttpEntity<String> request = 
+					new HttpEntity<>(requestBody.toString(), headers);
+			String result = restTemplate.postForObject(properties.getNotification().getPushe().getUrl(),
+					request, String.class);
+			log.info("Notification sent! {}", notificationDTOs[i]);
+			log.info("Notification Result {}", result);
+		}
 	}
 
 	@Override
 	@JmsListener(destination = "PUSHE_NOTIFICATIONBOX")
 	public void onMessage(Message message) {
 		try {
-			ObjectMessage objectMessage = (ObjectMessage) message;
-			NotificationDTO notificationDTO = (NotificationDTO) objectMessage.getObject();
-			deliverNotification(notificationDTO);
+			var objectMessage = (ObjectMessage) message;
+			var notificationDTO = (NotificationDTO) objectMessage.getObject();
+			this.deliverMessage(notificationDTO);
 		} catch (JMSException e) {
 			log.error("Parsing message failed: {}", e.getMessage(), e);
 		}
