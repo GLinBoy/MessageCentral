8a4a05afa91b6056d6831d7a349cf36de5245532
GLinBoy <hojjatabedie@gmail.com>
2021-05-28 15:18:43 +0430

    👌 IMPROVE: Extend Kavehnegar SMS Provider
    resolved #153

--- src/main/java/com/glinboy/app/service/impl/KavenegarSMSProviderServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/KavenegarSMSProviderServiceImpl.java
@@ -1,14 +1,9 @@
 package com.glinboy.app.service.impl;
 
-import java.util.List;
-
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
@@ -24,50 +19,44 @@ import com.kavenegar.sdk.models.SendResult;
 
 @Service
 @ConditionalOnProperty(value = "application.sms.provider", havingValue = "kavenegar")
-public class KavenegarSMSProviderServiceImpl implements ShortMessageProviderService<ShortMessageDTO>, MessageListener {
-
-	private final Logger log = LoggerFactory.getLogger(KavenegarSMSProviderServiceImpl.class);
-
-	private final ApplicationProperties properties;
-
-	private final JmsTemplate jmsTemplate;
-
-	public KavenegarSMSProviderServiceImpl(JmsTemplate jmsTemplate, ApplicationProperties properties) {
-		this.jmsTemplate = jmsTemplate;
-		this.properties = properties;
-	}
-
-	@Override
-	public void sendSMS(ShortMessageDTO shortMessageDTO) {
-		jmsTemplate.convertAndSend("KAVENEGAR_SMSBOX", shortMessageDTO);
-	}
+public class KavenegarSMSProviderServiceImpl extends GenericChannelServiceImpl<ShortMessageDTO> implements ShortMessageProviderService<ShortMessageDTO> {
 	
-	@Override
-	public void sendSMS(List<ShortMessageDTO> shortMessagesDTO) {
-		shortMessagesDTO.forEach(s -> jmsTemplate.convertAndSend("KAVENEGAR_SMSBOX", s));
+	public static final String TOPIC_NAME = "KAVENEGAR_SMSBOX";
+
+	protected KavenegarSMSProviderServiceImpl(JmsTemplate jmsTemplate,
+			ApplicationProperties properties) {
+		super(jmsTemplate, properties);
 	}
 
-	private void deliverSMS(ShortMessageDTO shortMessageDTO) {
-		try {
-			KavenegarApi api = new KavenegarApi(properties.getCredential().getKavenegar().getToken());
-			SendResult result = api.send(properties.getSms().getFrom(),
-					shortMessageDTO.getPhoneNumber(), shortMessageDTO.getContent());
-			log.info("SMS sent! {}", shortMessageDTO);
-			log.info("SMS Result {}", result);
-		} catch (HttpException ex) { // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
-			log.error("HttpException: {}", ex.getMessage(), ex);
-		} catch (ApiException ex) { // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
-			log.error("ApiException: {}", ex.getMessage(), ex);
+	@Override
+	String getTopicName() {
+		return TOPIC_NAME;
+	}
+
+	@Override
+	public void deliverMessage(ShortMessageDTO... shortMessageDTOs) {
+		var api = new KavenegarApi(properties.getCredential().getKavenegar().getToken());
+		for (var i = 0; i < shortMessageDTOs.length; i++) {
+			try {
+				SendResult result = api.send(properties.getSms().getFrom(),
+						shortMessageDTOs[i].getPhoneNumber(), shortMessageDTOs[i].getContent());
+				log.info("SMS sent! {}", shortMessageDTOs[i]);
+				log.info("SMS Result {}", result);
+			} catch (HttpException ex) { // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
+				log.error("HttpException: {}", ex.getMessage(), ex);
+			} catch (ApiException ex) { // در صورتی که خروجی وب سرویس 200 نباشد این خطارخ می دهد.
+				log.error("ApiException: {}", ex.getMessage(), ex);
+			}
 		}
 	}
 
 	@Override
-	@JmsListener(destination = "KAVENEGAR_SMSBOX")
+	@JmsListener(destination = KavenegarSMSProviderServiceImpl.TOPIC_NAME)
 	public void onMessage(Message message) {
 		try {
-			ObjectMessage objectMessage = (ObjectMessage) message;
-			ShortMessageDTO shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
-			deliverSMS(shortMessageDTO);
+			var objectMessage = (ObjectMessage) message;
+			var shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
+			this.deliverMessage(shortMessageDTO);
 		} catch (JMSException e) {
 			log.error("Parsing message failed: {}", e.getMessage(), e);
 		}
