3b7f66c98f5e3f39a47489079a558ae1c20f6c5f
GLinBoy <hojjatabedie@gmail.com>
2021-05-28 15:26:18 +0430

    👌 IMPROVE: Onesignal use GenericChannelServiceImpl
    resolved #156

--- src/main/java/com/glinboy/app/service/impl/OnesignalNotificationProviderServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/OnesignalNotificationProviderServiceImpl.java
@@ -6,14 +6,11 @@ import java.util.stream.Collectors;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -30,65 +27,58 @@ import com.glinboy.app.service.dto.NotificationDataDTO;
 
 @Service
 @ConditionalOnProperty(value = "application.notification.provider", havingValue = "onesignal", matchIfMissing = true)
-public class OnesignalNotificationProviderServiceImpl
-	implements NotificationProviderService<NotificationDTO>, MessageListener {
-
-	private final Logger log = LoggerFactory.getLogger(OnesignalNotificationProviderServiceImpl.class);
+public class OnesignalNotificationProviderServiceImpl extends GenericChannelServiceImpl<NotificationDTO>
+	implements NotificationProviderService<NotificationDTO> {
 	
-	private final ApplicationProperties properties;
+	public static final String TOPIC_NAME = "ONESIGNAL_NOTIFICATIONBOX";
 
-	private final JmsTemplate jmsTemplate;
-
-	public OnesignalNotificationProviderServiceImpl(ApplicationProperties properties,
-			JmsTemplate jmsTemplate) {
-		this.properties = properties;
-		this.jmsTemplate = jmsTemplate;
+	protected OnesignalNotificationProviderServiceImpl(JmsTemplate jmsTemplate,
+			ApplicationProperties properties) {
+		super(jmsTemplate, properties);
 	}
 
 	@Override
-	public void sendNotification(NotificationDTO notificationDTO) {
-		jmsTemplate.convertAndSend("ONESIGNAL_NOTIFICATIONBOX", notificationDTO);
+	String getTopicName() {
+		return TOPIC_NAME;
 	}
 
 	@Override
-	public void sendNotification(List<NotificationDTO> notificationDTOs) {
-		notificationDTOs.forEach(n -> jmsTemplate.convertAndSend("ONESIGNAL_NOTIFICATIONBOX", n));
-	}
-	
-	private void deliverNotification(NotificationDTO notificationDTO) {
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.setAcceptCharset(List.of(StandardCharsets.UTF_8));
-		JSONObject requestBody = new JSONObject();
-		requestBody.put("app_id", properties.getNotification().getOnesignal().getAppId());
-		requestBody.put("include_player_ids", new JSONArray(List.of(notificationDTO.getToken())));
-		JSONObject headings = new JSONObject();
-		headings.put("en", notificationDTO.getSubject());
-		requestBody.put("headings", headings);
-		JSONObject contents = new JSONObject();
-		contents.put("en", notificationDTO.getContent());
-		requestBody.put("contents", contents);
-		if(!CollectionUtils.isEmpty(notificationDTO.getData())) {
-			requestBody.put("data", notificationDTO.getData().stream()
-					.collect(Collectors.toMap(NotificationDataDTO::getKey,
-							NotificationDataDTO::getValue)));
+	public void deliverMessage(NotificationDTO... notificationDTOs) {
+		for ( var i = 0; i < notificationDTOs.length; i++) {
+			var restTemplate = new RestTemplate();
+			var headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.setAcceptCharset(List.of(StandardCharsets.UTF_8));
+			var requestBody = new JSONObject();
+			requestBody.put("app_id", properties.getNotification().getOnesignal().getAppId());
+			requestBody.put("include_player_ids", new JSONArray(List.of(notificationDTOs[i].getToken())));
+			var headings = new JSONObject();
+			headings.put("en", notificationDTOs[i].getSubject());
+			requestBody.put("headings", headings);
+			var contents = new JSONObject();
+			contents.put("en", notificationDTOs[i].getContent());
+			requestBody.put("contents", contents);
+			if(!CollectionUtils.isEmpty(notificationDTOs[i].getData())) {
+				requestBody.put("data", notificationDTOs[i].getData().stream()
+						.collect(Collectors.toMap(NotificationDataDTO::getKey,
+								NotificationDataDTO::getValue)));
+			}
+			HttpEntity<String> request = 
+					new HttpEntity<>(requestBody.toString(), headers);
+			String result = restTemplate.postForObject(properties.getNotification().getOnesignal().getUrl(),
+					request, String.class);
+			log.info("Notification sent! {}", notificationDTOs[i]);
+			log.info("Notification Result {}", result);
 		}
-		HttpEntity<String> request = 
-				new HttpEntity<>(requestBody.toString(), headers);
-		String result = restTemplate.postForObject(properties.getNotification().getOnesignal().getUrl(),
-				request, String.class);
-		log.info("Notification sent! {}", notificationDTO);
-		log.info("Notification Result {}", result);
 	}
 
 	@Override
-	@JmsListener(destination = "ONESIGNAL_NOTIFICATIONBOX")
+	@JmsListener(destination = OnesignalNotificationProviderServiceImpl.TOPIC_NAME)
 	public void onMessage(Message message) {
 		try {
-			ObjectMessage objectMessage = (ObjectMessage) message;
-			NotificationDTO notificationDTO = (NotificationDTO) objectMessage.getObject();
-			deliverNotification(notificationDTO);
+			var objectMessage = (ObjectMessage) message;
+			var notificationDTO = (NotificationDTO) objectMessage.getObject();
+			this.deliverMessage(notificationDTO);
 		} catch (JMSException e) {
 			log.error("Parsing message failed: {}", e.getMessage(), e);
 		}
