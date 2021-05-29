b88fb73aaa16554a8b076ee935fbf1700cd43dda
GLinBoy <hojjatabedie@gmail.com>
2021-05-29 16:21:09 +0430

    👌 IMPROVE: Rename ShortMessageProviderService
    #160

---
+++ src/main/java/com/glinboy/app/service/ShortMessageChannelService.java
@@ -0,0 +1,6 @@
+package com.glinboy.app.service;
+
+import com.glinboy.app.service.dto.ShortMessageDTO;
+
+public interface ShortMessageChannelService<T extends ShortMessageDTO> extends ChannelService<T> {
+}
--- src/main/java/com/glinboy/app/service/ShortMessageProviderService.java
+++
@@ -1,6 +0,0 @@
-package com.glinboy.app.service;
-
-import com.glinboy.app.service.dto.ShortMessageDTO;
-
-public interface ShortMessageProviderService<T extends ShortMessageDTO> extends ChannelService<T> {
-}
