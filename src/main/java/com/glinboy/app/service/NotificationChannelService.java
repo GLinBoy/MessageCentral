bdec02540a25a8859bd223f4a53be1149c7e8a56
GLinBoy <hojjatabedie@gmail.com>
2021-05-29 16:27:33 +0430

    👌 IMPROVE: Rename NotificationProviderService
    #161

---
+++ src/main/java/com/glinboy/app/service/NotificationChannelService.java
@@ -0,0 +1,6 @@
+package com.glinboy.app.service;
+
+import com.glinboy.app.service.dto.NotificationDTO;
+
+public interface NotificationChannelService<T extends NotificationDTO> extends ChannelService<T> {
+}
--- src/main/java/com/glinboy/app/service/NotificationProviderService.java
+++
@@ -1,6 +0,0 @@
-package com.glinboy.app.service;
-
-import com.glinboy.app.service.dto.NotificationDTO;
-
-public interface NotificationProviderService<T extends NotificationDTO> extends ChannelService<T> {
-}
