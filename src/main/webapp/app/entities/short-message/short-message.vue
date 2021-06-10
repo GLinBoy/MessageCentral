1516443b6718c3c79863f96595eb01e095e71e25
GLinBoy <hojjatabedie@gmail.com>
2021-06-10 14:45:09 +0430

    📦 NEW: Connect search component to service in SMS
    resolved #134

--- src/main/webapp/app/entities/short-message/short-message.vue
+++ src/main/webapp/app/entities/short-message/short-message.vue
@@ -9,10 +9,15 @@
       <div class="col-xs-12 col-md-8 col-lg-4">
         <div class="d-flex justify-content-end">
           <b-input-group class="mr-2">
-            <b-form-input type="text" :placeholder="$t('messageCentralApp.shortMessage.home.searchPlaceholder')" />
+            <b-form-input
+              type="text"
+              v-model="currentSearch"
+              @keydown.enter.native="handleSearch"
+              :placeholder="$t('messageCentralApp.shortMessage.home.searchPlaceholder')"
+            />
 
             <b-input-group-append>
-              <b-button variant="outline-primary">
+              <b-button variant="outline-primary" v-on:click="handleSearch">
                 <font-awesome-icon icon="search"></font-awesome-icon>
               </b-button>
             </b-input-group-append>
