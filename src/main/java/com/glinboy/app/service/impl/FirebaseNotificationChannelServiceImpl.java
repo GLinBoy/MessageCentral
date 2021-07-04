18fd415697a43a778a42b232a1828a9c526b5377
GLinBoy <hojjatabedie@gmail.com>
2021-07-04 21:01:33 +0430

    👌 IMPROVE: Update Notification status in Firebase impl
    #224

--- src/main/java/com/glinboy/app/service/impl/FirebaseNotificationChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/FirebaseNotificationChannelServiceImpl.java
@@ -13,7 +13,9 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.glinboy.app.config.ApplicationProperties;
+import com.glinboy.app.domain.enumeration.MessageStatus;
 import com.glinboy.app.service.NotificationChannelService;
+import com.glinboy.app.service.NotificationService;
 import com.glinboy.app.service.dto.NotificationDTO;
 import com.glinboy.app.service.dto.NotificationDataDTO;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -24,61 +26,67 @@ import com.google.firebase.messaging.Notification;
 @Service
 @ConditionalOnProperty(value = "application.notification.provider", havingValue = "firebase")
 public class FirebaseNotificationChannelServiceImpl extends GenericChannelServiceImpl<NotificationDTO>
-		implements NotificationChannelService<NotificationDTO> {
+        implements NotificationChannelService<NotificationDTO> {
 
-	public static final String TOPIC_NAME = "FIREBASE_NOTIFICATIONBOX";
+    public static final String TOPIC_NAME = "FIREBASE_NOTIFICATIONBOX";
 
-	private final FirebaseMessaging firebaseMessaging;
+    private final FirebaseMessaging firebaseMessaging;
 
-	protected FirebaseNotificationChannelServiceImpl(JmsTemplate jmsTemplate,
-			ApplicationProperties properties,
-			FirebaseMessaging firebaseMessaging) {
-		super(jmsTemplate, properties);
-		this.firebaseMessaging = firebaseMessaging;
-	}
+    private final NotificationService notificationService;
 
-	@Override
-	String getTopicName() {
-		return TOPIC_NAME;
-	}
+    protected FirebaseNotificationChannelServiceImpl(JmsTemplate jmsTemplate,
+            ApplicationProperties properties,
+            FirebaseMessaging firebaseMessaging,
+            NotificationService notificationService) {
+        super(jmsTemplate, properties);
+        this.firebaseMessaging = firebaseMessaging;
+        this.notificationService = notificationService;
+    }
 
-	@Override
-	public void deliverMessage(NotificationDTO... notificationDTOs) {
-		for (var i = 0; i < notificationDTOs.length; i++) {
-			try {
-				Notification notification = Notification.builder()
-						.setTitle(notificationDTOs[i].getSubject())
-						.setBody(notificationDTOs[i].getContent())
-						.build();
-	
-				Builder message = com.google.firebase.messaging.Message.builder()
-						.setToken(notificationDTOs[i].getToken())
-						.setNotification(notification);
-				if(!CollectionUtils.isEmpty(notificationDTOs[i].getData())) {
-					message.putAllData(notificationDTOs[i].getData().stream()
-							.collect(Collectors.toMap(NotificationDataDTO::getKey,
-									NotificationDataDTO::getValue)));
-				}
-	
-				String result = firebaseMessaging.send(message.build());
-				log.info("Notification sent! {}", notificationDTOs[i]);
-				log.info("Notification Result {}", result);
-			} catch (FirebaseMessagingException ex) {
-				log.error("Sending message failed: {}", ex.getMessage(), ex);
-			}
-		}
-	}
+    @Override
+    String getTopicName() {
+        return TOPIC_NAME;
+    }
 
-	@Override
-	@JmsListener(destination = FirebaseNotificationChannelServiceImpl.TOPIC_NAME)
-	public void onMessage(Message message) {
-		try {
-			var objectMessage = (ObjectMessage) message;
-			var notificationDTO = (NotificationDTO) objectMessage.getObject();
-			this.deliverMessage(notificationDTO);
-		} catch (JMSException e) {
-			log.error("Parsing message failed: {}", e.getMessage(), e);
-		}
-	}
+    @Override
+    public void deliverMessage(NotificationDTO... notificationDTOs) {
+        for (var i = 0; i < notificationDTOs.length; i++) {
+            try {
+                NotificationDTO notificationDTO = notificationDTOs[i];
+                Notification notification = Notification.builder()
+                        .setTitle(notificationDTO.getSubject())
+                        .setBody(notificationDTO.getContent())
+                        .build();
+
+                Builder message = com.google.firebase.messaging.Message.builder()
+                        .setToken(notificationDTO.getToken())
+                        .setNotification(notification);
+                if (!CollectionUtils.isEmpty(notificationDTO.getData())) {
+                    message.putAllData(notificationDTO.getData().stream()
+                            .collect(Collectors.toMap(NotificationDataDTO::getKey, NotificationDataDTO::getValue)));
+                }
+
+                String result = firebaseMessaging.send(message.build());
+                notificationDTO.setStatus(MessageStatus.SENT);
+                log.info("Notification sent! {}", notificationDTO);
+                log.info("Notification Result {}", result);
+                notificationService.save(notificationDTO);
+            } catch (FirebaseMessagingException ex) {
+                log.error("Sending message failed: {}", ex.getMessage(), ex);
+            }
+        }
+    }
+
+    @Override
+    @JmsListener(destination = FirebaseNotificationChannelServiceImpl.TOPIC_NAME)
+    public void onMessage(Message message) {
+        try {
+            var objectMessage = (ObjectMessage) message;
+            var notificationDTO = (NotificationDTO) objectMessage.getObject();
+            this.deliverMessage(notificationDTO);
+        } catch (JMSException e) {
+            log.error("Parsing message failed: {}", e.getMessage(), e);
+        }
+    }
 
 }
