82ebc93deeae2d34ed03984a7e33892ed8742ce9
GLinBoy <hojjatabedie@gmail.com>
2021-07-04 20:54:45 +0430

    👌 IMPROVE: Change status in MailProvider
    #222

--- src/main/java/com/glinboy/app/service/impl/MailChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/MailChannelServiceImpl.java
@@ -14,55 +14,67 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.glinboy.app.config.ApplicationProperties;
+import com.glinboy.app.domain.enumeration.MessageStatus;
+import com.glinboy.app.service.EmailService;
 import com.glinboy.app.service.MailChannelService;
 import com.glinboy.app.service.dto.EmailDTO;
 
 @Service
 @ConditionalOnProperty(value = "application.email.provider", havingValue = "mail-server", matchIfMissing = true)
-public class MailChannelServiceImpl extends GenericChannelServiceImpl<EmailDTO> implements MailChannelService<EmailDTO> {
-	
-	public static final String TOPIC_NAME = "MAILBOX";
+public class MailChannelServiceImpl extends GenericChannelServiceImpl<EmailDTO>
+        implements MailChannelService<EmailDTO> {
 
-	private final JavaMailSender emailSender;
+    public static final String TOPIC_NAME = "MAILBOX";
 
-	public MailChannelServiceImpl(JmsTemplate jmsTemplate,
-			ApplicationProperties properties,
-			JavaMailSender emailSender) {
-		super(jmsTemplate, properties);
-		this.emailSender = emailSender;
-	}
+    private final JavaMailSender emailSender;
 
-	@Override
-	String getTopicName() {
-		return TOPIC_NAME;
-	}
-	
-	@Override
-	public void deliverMessage(EmailDTO... emailsDTO) {
-		var messages = new SimpleMailMessage[emailsDTO.length];
-		for(var i = 0; i < emailsDTO.length; i++) {
-			var emailDTO = emailsDTO[i];
-			var message = new SimpleMailMessage();
-			message.setFrom(properties.getEmail().getFrom());
-			message.setTo(emailDTO.getReceiver());
-			message.setSubject(emailDTO.getSubject());
-			message.setText(emailDTO.getContent());
-			messages[i] = message;
-		}
-		emailSender.send(messages);
-		log.info("Mail(s) sent! {}", Arrays.toString(emailsDTO));
-	}
+    private final EmailService emailService;
 
-	@Override
-	@JmsListener(destination = MailChannelServiceImpl.TOPIC_NAME)
-	public void onMessage(Message message) {
-		try {
-			var objectMessage = (ObjectMessage)message;
-			var emailDTO = (EmailDTO)objectMessage.getObject();
-			this.deliverMessage(emailDTO);
-		} catch (JMSException e) {
-			log.error("Parsing message failed: {}", e.getMessage(), e);
-		}
-	}
+    public MailChannelServiceImpl(JmsTemplate jmsTemplate,
+            ApplicationProperties properties,
+            JavaMailSender emailSender,
+            EmailService emailService) {
+        super(jmsTemplate, properties);
+        this.emailSender = emailSender;
+        this.emailService = emailService;
+    }
+
+    @Override
+    String getTopicName() {
+        return TOPIC_NAME;
+    }
+
+    @Override
+    public void deliverMessage(EmailDTO... emailsDTO) {
+        var messages = new SimpleMailMessage[emailsDTO.length];
+        for (var i = 0; i < emailsDTO.length; i++) {
+            var emailDTO = emailsDTO[i];
+            var message = new SimpleMailMessage();
+            message.setFrom(properties.getEmail().getFrom());
+            message.setTo(emailDTO.getReceiver());
+            message.setSubject(emailDTO.getSubject());
+            message.setText(emailDTO.getContent());
+            messages[i] = message;
+        }
+        emailSender.send(messages);
+        for(int i = 0; i < emailsDTO.length; i++) {
+            var emailDTO = emailsDTO[i];
+            emailDTO.setStatus(MessageStatus.SENT);
+            this.emailService.partialUpdate(emailDTO);
+        }
+        log.info("Mail(s) sent! {}", Arrays.toString(emailsDTO));
+    }
+
+    @Override
+    @JmsListener(destination = MailChannelServiceImpl.TOPIC_NAME)
+    public void onMessage(Message message) {
+        try {
+            var objectMessage = (ObjectMessage) message;
+            var emailDTO = (EmailDTO) objectMessage.getObject();
+            this.deliverMessage(emailDTO);
+        } catch (JMSException e) {
+            log.error("Parsing message failed: {}", e.getMessage(), e);
+        }
+    }
 
 }
