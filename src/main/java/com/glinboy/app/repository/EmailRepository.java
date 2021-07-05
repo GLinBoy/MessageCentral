5331cd015d1e850c1a5869c015f7ac4ccba5601a
GLinBoy <hojjatabedie@gmail.com>
2021-07-05 17:14:59 +0430

    📦 NEW: Added update status to Email repository

--- src/main/java/com/glinboy/app/repository/EmailRepository.java
+++ src/main/java/com/glinboy/app/repository/EmailRepository.java
@@ -1,12 +1,20 @@
 package com.glinboy.app.repository;
 
 import com.glinboy.app.domain.Email;
+import com.glinboy.app.domain.enumeration.MessageStatus;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data SQL repository for the Email entity.
  */
-@SuppressWarnings("unused")
 @Repository
-public interface EmailRepository extends JpaRepository<Email, Long>, JpaSpecificationExecutor<Email> {}
+public interface EmailRepository extends JpaRepository<Email, Long>, JpaSpecificationExecutor<Email> {
+
+    @Modifying
+    @Query("update Email e set e.status = :status where e.id = :id")
+    void updateStatus(@Param(value = "id") long id, @Param(value = "status") MessageStatus status);
+
+}
