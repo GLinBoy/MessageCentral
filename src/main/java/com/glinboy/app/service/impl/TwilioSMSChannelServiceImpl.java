a9fa447e3afb663504dcc69a2465d4c1ccccb360
GLinBoy <hojjatabedie@gmail.com>
2021-07-05 16:53:41 +0430

    👌 IMPROVE: Update SMS status in Twilio

--- src/main/java/com/glinboy/app/service/impl/TwilioSMSChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/TwilioSMSChannelServiceImpl.java
@@ -10,51 +10,62 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.glinboy.app.config.ApplicationProperties;
+import com.glinboy.app.domain.enumeration.MessageStatus;
 import com.glinboy.app.service.ShortMessageChannelService;
+import com.glinboy.app.service.ShortMessageService;
 import com.glinboy.app.service.dto.ShortMessageDTO;
 import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
 
 @Service
 @ConditionalOnProperty(value = "application.sms.provider", havingValue = "twilio", matchIfMissing = true)
-public class TwilioSMSChannelServiceImpl extends GenericChannelServiceImpl<ShortMessageDTO> implements ShortMessageChannelService<ShortMessageDTO> {
-	
-	public static final String TOPIC_NAME = "TWILIO_SMSBOX";
+public class TwilioSMSChannelServiceImpl extends GenericChannelServiceImpl<ShortMessageDTO>
+        implements ShortMessageChannelService<ShortMessageDTO> {
 
-	protected TwilioSMSChannelServiceImpl(JmsTemplate jmsTemplate,
-			ApplicationProperties properties) {
-		super(jmsTemplate, properties);
-	}
+    public static final String TOPIC_NAME = "TWILIO_SMSBOX";
 
-	@Override
-	String getTopicName() {
-		return TOPIC_NAME;
-	}
+    public final ShortMessageService shortMessageService;
 
-	@Override
-	public void deliverMessage(ShortMessageDTO... shortMessageDTOs) {
-		Twilio.init(properties.getCredential().getTwilio().getAccountSid(),
-				properties.getCredential().getTwilio().getToken());
-		for (var i = 0; i < shortMessageDTOs.length; i++) {
-			var message = com.twilio.rest.api.v2010.account.Message
-					.creator(new PhoneNumber(shortMessageDTOs[i].getPhoneNumber()),
-							new PhoneNumber(properties.getSms().getFrom()), shortMessageDTOs[i].getContent())
-					.create();
-			log.info("SMS sent! {}", shortMessageDTOs[i]);
-			log.info("SMS Result {}", message);
-		}
-	}
+    protected TwilioSMSChannelServiceImpl(JmsTemplate jmsTemplate,
+            ApplicationProperties properties,
+            ShortMessageService shortMessageService) {
+        super(jmsTemplate, properties);
+        this.shortMessageService = shortMessageService;
+    }
 
-	@Override
-	@JmsListener(destination = TwilioSMSChannelServiceImpl.TOPIC_NAME)
-	public void onMessage(Message message) {
-		try {
-			var objectMessage = (ObjectMessage) message;
-			var shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
-			this.deliverMessage(shortMessageDTO);
-		} catch (JMSException e) {
-			log.error("Parsing message failed: {}", e.getMessage(), e);
-		}
-	}
+    @Override
+    String getTopicName() {
+        return TOPIC_NAME;
+    }
+
+    @Override
+    public void deliverMessage(ShortMessageDTO... shortMessageDTOs) {
+        Twilio.init(properties.getCredential().getTwilio().getAccountSid(),
+                properties.getCredential().getTwilio().getToken());
+        for (var i = 0; i < shortMessageDTOs.length; i++) {
+            ShortMessageDTO shortMessageDTO = shortMessageDTOs[i];
+            var message = com.twilio.rest.api.v2010.account.Message
+                    .creator(new PhoneNumber(shortMessageDTO.getPhoneNumber()),
+                            new PhoneNumber(properties.getSms().getFrom()),
+                            shortMessageDTO.getContent())
+                    .create();
+            log.info("SMS sent! {}", shortMessageDTO);
+            log.info("SMS Result {}", message);
+            shortMessageDTO.setStatus(MessageStatus.SENT);
+            this.shortMessageService.partialUpdate(shortMessageDTO);
+        }
+    }
+
+    @Override
+    @JmsListener(destination = TwilioSMSChannelServiceImpl.TOPIC_NAME)
+    public void onMessage(Message message) {
+        try {
+            var objectMessage = (ObjectMessage) message;
+            var shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
+            this.deliverMessage(shortMessageDTO);
+        } catch (JMSException e) {
+            log.error("Parsing message failed: {}", e.getMessage(), e);
+        }
+    }
 
 }
