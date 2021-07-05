8095cb2e200333f8a49534dbef28a64f4fb58353
GLinBoy <hojjatabedie@gmail.com>
2021-07-05 17:18:47 +0430

    📦 NEW: Added update status to Notification repository

--- src/main/java/com/glinboy/app/repository/NotificationRepository.java
+++ src/main/java/com/glinboy/app/repository/NotificationRepository.java
@@ -1,12 +1,23 @@
 package com.glinboy.app.repository;
 
-import com.glinboy.app.domain.Notification;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.glinboy.app.domain.Notification;
+import com.glinboy.app.domain.enumeration.MessageStatus;
 
 /**
  * Spring Data SQL repository for the Notification entity.
  */
-@SuppressWarnings("unused")
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long>, JpaSpecificationExecutor<Notification> {}
+public interface NotificationRepository extends JpaRepository<Notification, Long>, JpaSpecificationExecutor<Notification> {
+
+    @Modifying
+    @Query("update Notification n set n.status = :status where n.id = :id")
+    void updateStatus(@Param(value = "id") long id, @Param(value = "status") MessageStatus status);
+
+}
