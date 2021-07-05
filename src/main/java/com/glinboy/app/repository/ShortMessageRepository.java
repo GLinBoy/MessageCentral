6fcee4935a5548741d9987a1931e718c278a33d9
GLinBoy <hojjatabedie@gmail.com>
2021-07-05 17:17:05 +0430

    📦 NEW: Added update status to SMS repository

--- src/main/java/com/glinboy/app/repository/ShortMessageRepository.java
+++ src/main/java/com/glinboy/app/repository/ShortMessageRepository.java
@@ -1,12 +1,20 @@
 package com.glinboy.app.repository;
 
 import com.glinboy.app.domain.ShortMessage;
+import com.glinboy.app.domain.enumeration.MessageStatus;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data SQL repository for the ShortMessage entity.
  */
-@SuppressWarnings("unused")
 @Repository
-public interface ShortMessageRepository extends JpaRepository<ShortMessage, Long>, JpaSpecificationExecutor<ShortMessage> {}
+public interface ShortMessageRepository extends JpaRepository<ShortMessage, Long>, JpaSpecificationExecutor<ShortMessage> {
+
+    @Modifying
+    @Query("update ShortMessage s set s.status = :status where s.id = :id")
+    void updateStatus(@Param(value = "id") long id, @Param(value = "status") MessageStatus status);
+
+}
