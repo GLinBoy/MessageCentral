d013ea93d6429c4af6e4efc0c8c0d8698d17a397
GLinBoy <hojjatabedie@gmail.com>
2021-07-18 18:57:27 +0430

    📦 NEW: Added method to update status to FAILED
    #231

--- src/main/java/com/glinboy/app/service/ChannelService.java
+++ src/main/java/com/glinboy/app/service/ChannelService.java
@@ -2,8 +2,10 @@ package com.glinboy.app.service;
 
 public interface ChannelService<T> {
 
-	void sendMessage(T... ts);
-	
-	void updateStatusToSent(T... ts);
+    void sendMessage(T... ts);
+
+    void updateStatusToSent(T... ts);
+
+    void updateStatusToFailed(T... ts);
 
 }
