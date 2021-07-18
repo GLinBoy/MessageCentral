1e3cf7b71d1fe73e9bc6cc4c1d8e38d2fd4e2400
GLinBoy <hojjatabedie@gmail.com>
2021-07-18 19:58:44 +0430

    📦 NEW: Added updateStatusToSent to MailChannelService
    #233

--- src/main/java/com/glinboy/app/service/MailChannelService.java
+++ src/main/java/com/glinboy/app/service/MailChannelService.java
@@ -1,6 +1,18 @@
 package com.glinboy.app.service;
 
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import com.glinboy.app.domain.enumeration.MessageStatus;
 import com.glinboy.app.service.dto.EmailDTO;
 
 public interface MailChannelService<T extends EmailDTO> extends ChannelService<T> {
+
+    Consumer<T[]> saveFunction();
+
+    @Override
+    default void updateStatusToSent(T... ts) {
+        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
+        saveFunction().accept(ts);
+    }
 }
