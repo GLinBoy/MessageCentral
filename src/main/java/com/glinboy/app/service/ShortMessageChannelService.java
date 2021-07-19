a26716698212a7ccc0a399240b92b47af9779aac
GLinBoy <hojjatabedie@gmail.com>
2021-07-19 13:18:28 +0430

    📦 NEW: Added updateStatusToSent impl

--- src/main/java/com/glinboy/app/service/ShortMessageChannelService.java
+++ src/main/java/com/glinboy/app/service/ShortMessageChannelService.java
@@ -1,6 +1,18 @@
 package com.glinboy.app.service;
 
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import com.glinboy.app.domain.enumeration.MessageStatus;
 import com.glinboy.app.service.dto.ShortMessageDTO;
 
 public interface ShortMessageChannelService<T extends ShortMessageDTO> extends ChannelService<T> {
+
+    Consumer<T[]> saveFunction();
+
+    @Override
+    default void updateStatusToSent(T... ts) {
+        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
+        saveFunction().accept(ts);
+    }
 }
