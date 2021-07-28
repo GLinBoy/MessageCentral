517f444b353494bcf697116939d9d6a58f4d37c5
GLinBoy <hojjatabedie@gmail.com>
2021-07-28 13:22:03 +0430

    👌 IMPROVE: Remove @override

--- src/main/java/com/glinboy/app/service/MailChannelService.java
+++ src/main/java/com/glinboy/app/service/MailChannelService.java
@@ -10,13 +10,11 @@ public interface MailChannelService<T extends EmailDTO> extends ChannelService<T
 
     Consumer<T[]> saveFunction();
 
-    @Override
     default void updateStatusToSent(T... ts) {
         Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
         saveFunction().accept(ts);
     }
 
-    @Override
     default void updateStatusToFailed(T... ts) {
         Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.FAILED));
         saveFunction().accept(ts);
--- src/main/java/com/glinboy/app/service/NotificationChannelService.java
+++ src/main/java/com/glinboy/app/service/NotificationChannelService.java
@@ -10,13 +10,11 @@ public interface NotificationChannelService<T extends NotificationDTO> extends C
 
     Consumer<T[]> saveFunction();
 
-    @Override
     default void updateStatusToSent(T... ts) {
         Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
         saveFunction().accept(ts);
     }
 
-    @Override
     default void updateStatusToFailed(T... ts) {
         Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.FAILED));
         saveFunction().accept(ts);
