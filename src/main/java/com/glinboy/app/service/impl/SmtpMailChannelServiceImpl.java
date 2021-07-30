53376efa97db80f08cc3f23635762b181d51650b
GLinBoy <hojjatabedie@gmail.com>
2021-07-30 13:36:38 +0430

    👌 IMPROVE: Now end developer only implement a method, deliverMessage

--- src/main/java/com/glinboy/app/service/impl/SmtpMailChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/SmtpMailChannelServiceImpl.java
@@ -1,72 +1,32 @@
 package com.glinboy.app.service.impl;
 
 import java.util.Arrays;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.glinboy.app.config.ApplicationProperties;
-import com.glinboy.app.repository.EmailRepository;
 import com.glinboy.app.service.MailChannelService;
 import com.glinboy.app.service.dto.EmailDTO;
-import com.glinboy.app.service.mapper.EmailMapper;
 
 @Service
 @ConditionalOnProperty(value = "application.email.provider", havingValue = "mail-server", matchIfMissing = true)
-public class MailChannelServiceImpl extends GenericChannelServiceImpl<EmailDTO>
-        implements MailChannelService<EmailDTO> {
-
-    public static final String TOPIC_NAME = "MAILBOX";
+public class SmtpMailChannelServiceImpl extends MailChannelService {
 
     private final JavaMailSender emailSender;
 
-    private final EmailRepository emailRepository;
-
-    private final EmailMapper emailMapper;
-
-    private final ApplicationContext applicationContext;
-
-    public MailChannelServiceImpl(JmsTemplate jmsTemplate,
+    protected SmtpMailChannelServiceImpl(JmsTemplate jmsTemplate,
             ApplicationProperties properties,
-            JavaMailSender emailSender,
-            EmailRepository emailRepository,
-            EmailMapper emailMapper,
-            ApplicationContext applicationContext) {
+            JavaMailSender emailSender) {
         super(jmsTemplate, properties);
         this.emailSender = emailSender;
-        this.emailRepository = emailRepository;
-        this.emailMapper = emailMapper;
-        this.applicationContext = applicationContext;
     }
 
     @Override
-    public String getTopicName() {
-        return TOPIC_NAME;
-    }
-    
-    @Override
-    public Consumer<EmailDTO[]> saveFunction() {
-        return emails -> emailRepository.saveAll(Stream
-                .of(emails)
-                .map(emailMapper::toEntity)
-                .collect(Collectors.toList()));
-    }
-
-    @Override
-    public void deliverMessage(EmailDTO... emailsDTO) {
+    public void deliverMessage(EmailDTO... emailsDTO) throws Exception {
         var messages = new SimpleMailMessage[emailsDTO.length];
         for (var i = 0; i < emailsDTO.length; i++) {
             var emailDTO = emailsDTO[i];
@@ -78,21 +38,7 @@ public class MailChannelServiceImpl extends GenericChannelServiceImpl<EmailDTO>
             messages[i] = message;
         }
         emailSender.send(messages);
-        updateStatusToSent(emailsDTO);
         log.info("Mail(s) sent! {}", Arrays.toString(emailsDTO));
-    }
-
-    @Override
-    @Transactional
-    @JmsListener(destination = MailChannelServiceImpl.TOPIC_NAME)
-    public void onMessage(Message message) {
-        try {
-            var objectMessage = (ObjectMessage) message;
-            var emailDTO = (EmailDTO) objectMessage.getObject();
-            ((MailChannelServiceImpl) applicationContext.getBean(MailChannelServiceImpl.class)).deliverMessage(emailDTO);
-        } catch (JMSException e) {
-            log.error("Parsing message failed: {}", e.getMessage(), e);
-        }
     }
 
 }
