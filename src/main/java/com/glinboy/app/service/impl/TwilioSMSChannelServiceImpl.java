80bd3a6f98482766753b4d1435a2cab5ac7e0776
GLinBoy <hojjatabedie@gmail.com>
2021-07-30 13:48:16 +0430

    👌 IMPROVE: removed unnecessary method, only deliverMessage

--- src/main/java/com/glinboy/app/service/impl/TwilioSMSChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/TwilioSMSChannelServiceImpl.java
@@ -1,57 +1,22 @@
 package com.glinboy.app.service.impl;
 
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.glinboy.app.config.ApplicationProperties;
-import com.glinboy.app.repository.ShortMessageRepository;
 import com.glinboy.app.service.ShortMessageChannelService;
 import com.glinboy.app.service.dto.ShortMessageDTO;
-import com.glinboy.app.service.mapper.ShortMessageMapper;
 import com.twilio.Twilio;
 import com.twilio.type.PhoneNumber;
 
 @Service
 @ConditionalOnProperty(value = "application.sms.provider", havingValue = "twilio", matchIfMissing = true)
-public class TwilioSMSChannelServiceImpl extends GenericChannelServiceImpl<ShortMessageDTO>
-        implements ShortMessageChannelService<ShortMessageDTO> {
-
-    public static final String TOPIC_NAME = "TWILIO_SMSBOX";
-
-    public final ShortMessageRepository shortMessageRepository;
-    
-    public final ShortMessageMapper shortMessageMapper;
+public class TwilioSMSChannelServiceImpl extends ShortMessageChannelService {
 
     protected TwilioSMSChannelServiceImpl(JmsTemplate jmsTemplate,
-            ApplicationProperties properties,
-            ShortMessageRepository shortMessageRepository,
-            ShortMessageMapper shortMessageMapper) {
+            ApplicationProperties properties) {
         super(jmsTemplate, properties);
-        this.shortMessageRepository = shortMessageRepository;
-        this.shortMessageMapper = shortMessageMapper;
-    }
-
-    @Override
-    public String getTopicName() {
-        return TOPIC_NAME;
-    }
-    
-    @Override
-    public Consumer<ShortMessageDTO[]> saveFunction() {
-        return emails -> shortMessageRepository.saveAll(Stream
-                .of(emails)
-                .map(shortMessageMapper::toEntity)
-                .collect(Collectors.toList()));
     }
 
     @Override
@@ -67,19 +32,6 @@ public class TwilioSMSChannelServiceImpl extends GenericChannelServiceImpl<Short
                     .create();
             log.info("SMS sent! {}", shortMessageDTO);
             log.info("SMS Result {}", message);
-            this.updateStatusToSent(shortMessageDTOs);
-        }
-    }
-
-    @Override
-    @JmsListener(destination = TwilioSMSChannelServiceImpl.TOPIC_NAME)
-    public void onMessage(Message message) {
-        try {
-            var objectMessage = (ObjectMessage) message;
-            var shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
-            this.deliverMessage(shortMessageDTO);
-        } catch (JMSException e) {
-            log.error("Parsing message failed: {}", e.getMessage(), e);
         }
     }
 
