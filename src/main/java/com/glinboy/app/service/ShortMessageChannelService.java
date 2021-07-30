8f5906421801a6cd7bfc60737a72c9692eaa76d7
GLinBoy <hojjatabedie@gmail.com>
2021-07-30 13:47:13 +0430

    👌 IMPROVE: Change its type to abstract class to handle common business

--- src/main/java/com/glinboy/app/service/ShortMessageChannelService.java
+++ src/main/java/com/glinboy/app/service/ShortMessageChannelService.java
@@ -1,22 +1,53 @@
 package com.glinboy.app.service;
 
-import java.util.function.Consumer;
-import java.util.stream.Stream;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 
-import com.glinboy.app.domain.enumeration.MessageStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.glinboy.app.config.ApplicationProperties;
 import com.glinboy.app.service.dto.ShortMessageDTO;
 
-public interface ShortMessageChannelService<T extends ShortMessageDTO> extends ChannelService<T> {
+public abstract class ShortMessageChannelService implements ChannelService<ShortMessageDTO>, MessageListener {
 
-    Consumer<T[]> saveFunction();
 
-    default void updateStatusToSent(T... ts) {
-        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
-        saveFunction().accept(ts);
+    public static final String TOPIC_NAME = "TWILIO_SMSBOX";
+    
+    protected final Logger log;
+
+    protected final ApplicationProperties properties;
+
+    protected final JmsTemplate jmsTemplate;
+    
+    protected ShortMessageChannelService(JmsTemplate jmsTemplate,
+            ApplicationProperties properties) {
+        log = LoggerFactory.getLogger(this.getClass());
+        this.jmsTemplate = jmsTemplate;
+        this.properties = properties;
     }
 
-    default void updateStatusToFailed(T... ts) {
-        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.FAILED));
-        saveFunction().accept(ts);
+    public abstract void deliverMessage(ShortMessageDTO... shortMessageDTOs) throws Exception;
+
+    @Override
+    public void sendMessage(ShortMessageDTO... shortMessageDTOs) {
+        for(var i = 0; i < shortMessageDTOs.length; i++) {
+            jmsTemplate.convertAndSend(TOPIC_NAME, shortMessageDTOs[i]);
+        }
+    }
+
+    @Override
+    @JmsListener(destination = ShortMessageChannelService.TOPIC_NAME)
+    public void onMessage(Message message) {
+        try {
+            var objectMessage = (ObjectMessage) message;
+            var shortMessageDTO = (ShortMessageDTO) objectMessage.getObject();
+            deliverMessage(shortMessageDTO);
+        } catch (Exception e) {
+            log.error("Parsing message failed: {}", e.getMessage(), e);
+        }
     }
 }
