6ea8c82eb186e3554ec0e3a8a618d8b19fa75fa2
GLinBoy <hojjatabedie@gmail.com>
2021-07-30 13:35:11 +0430

    👌 IMPROVE: Change its type to abstract class to handle mail business

--- src/main/java/com/glinboy/app/service/MailChannelService.java
+++ src/main/java/com/glinboy/app/service/MailChannelService.java
@@ -1,22 +1,51 @@
 package com.glinboy.app.service;
 
-import java.util.function.Consumer;
-import java.util.stream.Stream;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
 
-import com.glinboy.app.domain.enumeration.MessageStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.glinboy.app.config.ApplicationProperties;
 import com.glinboy.app.service.dto.EmailDTO;
 
-public interface MailChannelService<T extends EmailDTO> extends ChannelService<T> {
+public abstract class MailChannelService implements ChannelService<EmailDTO>, MessageListener {
 
-    Consumer<T[]> saveFunction();
+    public static final String TOPIC_NAME = "MAILBOX";
+    
+    protected final Logger log;
 
-    default void updateStatusToSent(T... ts) {
-        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
-        saveFunction().accept(ts);
+    protected final ApplicationProperties properties;
+
+    protected final JmsTemplate jmsTemplate;
+
+    protected MailChannelService(JmsTemplate jmsTemplate, ApplicationProperties properties) {
+        log = LoggerFactory.getLogger(this.getClass());
+        this.jmsTemplate = jmsTemplate;
+        this.properties = properties;
     }
 
-    default void updateStatusToFailed(T... ts) {
-        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.FAILED));
-        saveFunction().accept(ts);
+    public abstract void deliverMessage(EmailDTO... emailDTOs) throws Exception;
+
+    @Override
+    public void sendMessage(EmailDTO... emailDTOs) {
+        for(var i = 0; i < emailDTOs.length; i++) {
+            jmsTemplate.convertAndSend(TOPIC_NAME, emailDTOs[i]);
+        }
+    }
+
+    @Override
+    @JmsListener(destination = MailChannelService.TOPIC_NAME)
+    public void onMessage(Message message) {
+        try {
+            var objectMessage = (ObjectMessage) message;
+            EmailDTO emailDTO = (EmailDTO) objectMessage.getObject();
+            this.deliverMessage(emailDTO);
+        } catch (Exception e) {
+            log.error("Parsing message failed: {}", e.getMessage(), e);
+        }
     }
 }
