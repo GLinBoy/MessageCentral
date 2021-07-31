895860d9cd53cdd3210bbe898a18f7ae5a129187
GLinBoy <hojjatabedie@gmail.com>
2021-07-31 12:17:01 +0430

    👌 IMPROVE: Change abstract class to Interface

--- src/main/java/com/glinboy/app/service/MailChannelService.java
+++ src/main/java/com/glinboy/app/service/MailChannelService.java
@@ -2,50 +2,13 @@ package com.glinboy.app.service;
 
 import javax.jms.Message;
 import javax.jms.MessageListener;
-import javax.jms.ObjectMessage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
-
-import com.glinboy.app.config.ApplicationProperties;
 import com.glinboy.app.service.dto.EmailDTO;
 
-public abstract class MailChannelService implements ChannelService<EmailDTO>, MessageListener {
+public interface MailChannelService<T extends EmailDTO> extends ChannelService<T>, MessageListener {
 
-    public static final String TOPIC_NAME = "MAILBOX";
-    
-    protected final Logger log;
+    void deliverMessage(T... ts) throws Exception;
 
-    protected final ApplicationProperties properties;
+    public void onMessage(Message message);
 
-    protected final JmsTemplate jmsTemplate;
-
-    protected MailChannelService(JmsTemplate jmsTemplate, ApplicationProperties properties) {
-        log = LoggerFactory.getLogger(this.getClass());
-        this.jmsTemplate = jmsTemplate;
-        this.properties = properties;
-    }
-
-    public abstract void deliverMessage(EmailDTO... emailDTOs) throws Exception;
-
-    @Override
-    public void sendMessage(EmailDTO... emailDTOs) {
-        for(var i = 0; i < emailDTOs.length; i++) {
-            jmsTemplate.convertAndSend(TOPIC_NAME, emailDTOs[i]);
-        }
-    }
-
-    @Override
-    @JmsListener(destination = MailChannelService.TOPIC_NAME)
-    public void onMessage(Message message) {
-        try {
-            var objectMessage = (ObjectMessage) message;
-            EmailDTO emailDTO = (EmailDTO) objectMessage.getObject();
-            this.deliverMessage(emailDTO);
-        } catch (Exception e) {
-            log.error("Parsing message failed: {}", e.getMessage(), e);
-        }
-    }
 }
