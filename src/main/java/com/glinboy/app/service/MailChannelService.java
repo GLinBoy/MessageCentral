d57af6b034c3afac1af4a56d0139e4c32100fc83
GLinBoy <hojjatabedie@gmail.com>
2021-07-31 12:36:38 +0430

    👌 IMPROVE: Removed unnecessary method

--- src/main/java/com/glinboy/app/service/MailChannelService.java
+++ src/main/java/com/glinboy/app/service/MailChannelService.java
@@ -1,6 +1,5 @@
 package com.glinboy.app.service;
 
-import javax.jms.Message;
 import javax.jms.MessageListener;
 
 import com.glinboy.app.service.dto.EmailDTO;
@@ -8,7 +7,5 @@ import com.glinboy.app.service.dto.EmailDTO;
 public interface MailChannelService<T extends EmailDTO> extends ChannelService<T>, MessageListener {
 
     void deliverMessage(T... ts) throws Exception;
-
-    public void onMessage(Message message);
 
 }
