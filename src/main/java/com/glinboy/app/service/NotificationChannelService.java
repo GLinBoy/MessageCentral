93d33f11b1d8ed4ef63125958799797da584fac8
GLinBoy <hojjatabedie@gmail.com>
2021-08-01 13:00:24 +0430

    👌 IMPROVE: Update methods

--- src/main/java/com/glinboy/app/service/NotificationChannelService.java
+++ src/main/java/com/glinboy/app/service/NotificationChannelService.java
@@ -1,22 +1,10 @@
 package com.glinboy.app.service;
 
-import java.util.function.Consumer;
-import java.util.stream.Stream;
+import javax.jms.MessageListener;
 
-import com.glinboy.app.domain.enumeration.MessageStatus;
 import com.glinboy.app.service.dto.NotificationDTO;
 
-public interface NotificationChannelService<T extends NotificationDTO> extends ChannelService<T> {
+public interface NotificationChannelService<T extends NotificationDTO> extends ChannelService<T>, MessageListener  {
 
-    Consumer<T[]> saveFunction();
-
-    default void updateStatusToSent(T... ts) {
-        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.SENT));
-        saveFunction().accept(ts);
-    }
-
-    default void updateStatusToFailed(T... ts) {
-        Stream.of(ts).forEach(t -> t.setStatus(MessageStatus.FAILED));
-        saveFunction().accept(ts);
-    }
+    void deliverMessage(T... ts) throws Exception;
 }
