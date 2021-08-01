120ee4396acc6b2a3b58e8e6dde454e4d9de1419
GLinBoy <hojjatabedie@gmail.com>
2021-08-01 13:12:14 +0430

    👌 IMPROVE: Removed unnecessary methods, keep only deliverMessage

--- src/main/java/com/glinboy/app/service/impl/FirebaseNotificationChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/FirebaseNotificationChannelServiceImpl.java
@@ -1,25 +1,15 @@
 package com.glinboy.app.service.impl;
 
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.glinboy.app.config.ApplicationProperties;
-import com.glinboy.app.repository.NotificationRepository;
-import com.glinboy.app.service.NotificationChannelService;
 import com.glinboy.app.service.dto.NotificationDTO;
 import com.glinboy.app.service.dto.NotificationDataDTO;
-import com.glinboy.app.service.mapper.NotificationMapper;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message.Builder;
@@ -27,39 +17,15 @@ import com.google.firebase.messaging.Notification;
 
 @Service
 @ConditionalOnProperty(value = "application.notification.provider", havingValue = "firebase")
-public class FirebaseNotificationChannelServiceImpl extends GenericChannelServiceImpl<NotificationDTO>
-        implements NotificationChannelService<NotificationDTO> {
-
-    public static final String TOPIC_NAME = "FIREBASE_NOTIFICATIONBOX";
+public class FirebaseNotificationChannelServiceImpl extends AbstractNotificationChannelServiceImpl {
 
     private final FirebaseMessaging firebaseMessaging;
 
-    private final NotificationRepository notificationRepository;
-
-    private final NotificationMapper notificationMapper;
-
     protected FirebaseNotificationChannelServiceImpl(JmsTemplate jmsTemplate,
             ApplicationProperties properties,
-            FirebaseMessaging firebaseMessaging,
-            NotificationRepository notificationRepository,
-            NotificationMapper notificationMapper) {
+            FirebaseMessaging firebaseMessaging) {
         super(jmsTemplate, properties);
         this.firebaseMessaging = firebaseMessaging;
-        this.notificationRepository = notificationRepository;
-        this.notificationMapper = notificationMapper;
-    }
-
-    @Override
-    public String getTopicName() {
-        return TOPIC_NAME;
-    }
-
-    @Override
-    public Consumer<NotificationDTO[]> saveFunction() {
-        return emails -> notificationRepository.saveAll(Stream
-                .of(emails)
-                .map(notificationMapper::toEntity)
-                .collect(Collectors.toList()));
     }
 
     @Override
@@ -83,22 +49,9 @@ public class FirebaseNotificationChannelServiceImpl extends GenericChannelServic
                 String result = firebaseMessaging.send(message.build());
                 log.info("Notification sent! {}", notificationDTO);
                 log.info("Notification Result {}", result);
-                updateStatusToSent(notificationDTO);
             } catch (FirebaseMessagingException ex) {
                 log.error("Sending message failed: {}", ex.getMessage(), ex);
             }
-        }
-    }
-
-    @Override
-    @JmsListener(destination = FirebaseNotificationChannelServiceImpl.TOPIC_NAME)
-    public void onMessage(Message message) {
-        try {
-            var objectMessage = (ObjectMessage) message;
-            var notificationDTO = (NotificationDTO) objectMessage.getObject();
-            this.deliverMessage(notificationDTO);
-        } catch (JMSException e) {
-            log.error("Parsing message failed: {}", e.getMessage(), e);
         }
     }
 
