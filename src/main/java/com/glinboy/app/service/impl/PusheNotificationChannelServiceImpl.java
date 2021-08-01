edd88c9ab4515ee1632a579adfbdee531eab4ea5
GLinBoy <hojjatabedie@gmail.com>
2021-08-01 13:12:03 +0430

    👌 IMPROVE: Removed unnecessary methods, keep only deliverMessage

--- src/main/java/com/glinboy/app/service/impl/PusheNotificationChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/PusheNotificationChannelServiceImpl.java
@@ -2,13 +2,6 @@ package com.glinboy.app.service.impl;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -16,48 +9,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.glinboy.app.config.ApplicationProperties;
-import com.glinboy.app.repository.NotificationRepository;
-import com.glinboy.app.service.NotificationChannelService;
 import com.glinboy.app.service.dto.NotificationDTO;
-import com.glinboy.app.service.mapper.NotificationMapper;
 
 @Service
 @ConditionalOnProperty(value = "application.notification.provider", havingValue = "pushe")
-public class PusheNotificationChannelServiceImpl extends GenericChannelServiceImpl<NotificationDTO>
-        implements NotificationChannelService<NotificationDTO> {
-
-    public static final String TOPIC_NAME = "PUSHE_NOTIFICATIONBOX";
-
-    public final NotificationRepository notificationRepository;
-    
-    private final NotificationMapper notificationMapper;
+public class PusheNotificationChannelServiceImpl extends AbstractNotificationChannelServiceImpl {
 
     protected PusheNotificationChannelServiceImpl(JmsTemplate jmsTemplate,
-            ApplicationProperties properties,
-            NotificationRepository notificationRepository,
-            NotificationMapper notificationMapper) {
+            ApplicationProperties properties) {
         super(jmsTemplate, properties);
-        this.notificationRepository = notificationRepository;
-        this.notificationMapper = notificationMapper;
-    }
-
-    @Override
-    public String getTopicName() {
-        return TOPIC_NAME;
-    }
-
-    @Override
-    public Consumer<NotificationDTO[]> saveFunction() {
-        return emails -> notificationRepository.saveAll(Stream
-                .of(emails)
-                .map(notificationMapper::toEntity)
-                .collect(Collectors.toList()));
     }
 
     @Override
@@ -83,19 +48,6 @@ public class PusheNotificationChannelServiceImpl extends GenericChannelServiceIm
                     String.class);
             log.info("Notification sent! {}", notificationDTO);
             log.info("Notification Result {}", result);
-            updateStatusToSent(notificationDTO);
-        }
-    }
-
-    @Override
-    @JmsListener(destination = "PUSHE_NOTIFICATIONBOX")
-    public void onMessage(Message message) {
-        try {
-            var objectMessage = (ObjectMessage) message;
-            var notificationDTO = (NotificationDTO) objectMessage.getObject();
-            this.deliverMessage(notificationDTO);
-        } catch (JMSException e) {
-            log.error("Parsing message failed: {}", e.getMessage(), e);
         }
     }
 
