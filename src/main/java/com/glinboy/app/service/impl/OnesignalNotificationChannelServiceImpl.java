a7ab2ff6b3dab51c2ba715f4099563f076a7189f
GLinBoy <hojjatabedie@gmail.com>
2021-08-01 13:12:08 +0430

    👌 IMPROVE: Removed unnecessary methods, keep only deliverMessage

--- src/main/java/com/glinboy/app/service/impl/OnesignalNotificationChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/OnesignalNotificationChannelServiceImpl.java
@@ -2,13 +2,7 @@ package com.glinboy.app.service.impl;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.json.JSONArray;
@@ -17,49 +11,21 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.glinboy.app.config.ApplicationProperties;
-import com.glinboy.app.repository.NotificationRepository;
-import com.glinboy.app.service.NotificationChannelService;
 import com.glinboy.app.service.dto.NotificationDTO;
 import com.glinboy.app.service.dto.NotificationDataDTO;
-import com.glinboy.app.service.mapper.NotificationMapper;
 
 @Service
 @ConditionalOnProperty(value = "application.notification.provider", havingValue = "onesignal", matchIfMissing = true)
-public class OnesignalNotificationChannelServiceImpl extends GenericChannelServiceImpl<NotificationDTO>
-        implements NotificationChannelService<NotificationDTO> {
-
-    public static final String TOPIC_NAME = "ONESIGNAL_NOTIFICATIONBOX";
-
-    public final NotificationRepository notificationRepository;
-    
-    private final NotificationMapper notificationMapper;
+public class OnesignalNotificationChannelServiceImpl extends AbstractNotificationChannelServiceImpl {
 
     protected OnesignalNotificationChannelServiceImpl(JmsTemplate jmsTemplate,
-            ApplicationProperties properties,
-            NotificationRepository notificationRepository,
-            NotificationMapper notificationMapper) {
+            ApplicationProperties properties) {
         super(jmsTemplate, properties);
-        this.notificationRepository = notificationRepository;
-        this.notificationMapper = notificationMapper;
-    }
-
-    @Override
-    public String getTopicName() {
-        return TOPIC_NAME;
-    }
-
-    @Override
-    public Consumer<NotificationDTO[]> saveFunction() {
-        return emails -> notificationRepository.saveAll(Stream
-                .of(emails)
-                .map(notificationMapper::toEntity)
-                .collect(Collectors.toList()));
     }
 
     @Override
@@ -88,19 +54,6 @@ public class OnesignalNotificationChannelServiceImpl extends GenericChannelServi
                     String.class);
             log.info("Notification sent! {}", notificationDTO);
             log.info("Notification Result {}", result);
-            updateStatusToSent(notificationDTO);
-        }
-    }
-
-    @Override
-    @JmsListener(destination = OnesignalNotificationChannelServiceImpl.TOPIC_NAME)
-    public void onMessage(Message message) {
-        try {
-            var objectMessage = (ObjectMessage) message;
-            var notificationDTO = (NotificationDTO) objectMessage.getObject();
-            this.deliverMessage(notificationDTO);
-        } catch (JMSException e) {
-            log.error("Parsing message failed: {}", e.getMessage(), e);
         }
     }
 
