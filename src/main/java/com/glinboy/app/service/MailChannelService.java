94acb6acb2b14067e0a6715f3ba84f871d7c6b5b
GLinBoy <hojjatabedie@gmail.com>
2021-08-04 16:13:27 +0430

    👌 IMPROVE: remove MessageListener interface to use @JmsListener

--- src/main/java/com/glinboy/app/service/MailChannelService.java
+++ src/main/java/com/glinboy/app/service/MailChannelService.java
@@ -1,10 +1,8 @@
 package com.glinboy.app.service;
 
-import javax.jms.MessageListener;
-
 import com.glinboy.app.service.dto.EmailDTO;
 
-public interface MailChannelService<T extends EmailDTO> extends ChannelService<T>, MessageListener {
+public interface MailChannelService<T extends EmailDTO> extends ChannelService<T> {
 
     void deliverMessage(T... ts) throws Exception;
 
