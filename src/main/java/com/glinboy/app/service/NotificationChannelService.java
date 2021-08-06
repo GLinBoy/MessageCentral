ef801e1048fbad263f196e03fcfe1f655cd6ab9e
GLinBoy <hojjatabedie@gmail.com>
2021-08-06 18:31:13 +0430

    👌 IMPROVE: Removed MessageListener and added onMessage method to it
    resolved #263

--- src/main/java/com/glinboy/app/service/NotificationChannelService.java
+++ src/main/java/com/glinboy/app/service/NotificationChannelService.java
@@ -1,10 +1,9 @@
 package com.glinboy.app.service;
 
-import javax.jms.MessageListener;
-
 import com.glinboy.app.service.dto.NotificationDTO;
 
-public interface NotificationChannelService<T extends NotificationDTO> extends ChannelService<T>, MessageListener  {
+public interface NotificationChannelService<T extends NotificationDTO> extends ChannelService<T> {
 
     void deliverMessage(T... ts) throws Exception;
+    void onMessage(T... ts);
 }
