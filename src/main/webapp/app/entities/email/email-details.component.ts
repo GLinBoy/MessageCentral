7da0a37628be100bcd0e16ae58b6e51d67bee94d
GLinBoy <hojjatabedie@gmail.com>
2021-08-18 15:54:11 +0430

    📦 NEW: Added getVariant function

--- src/main/webapp/app/entities/email/email-details.component.ts
+++ src/main/webapp/app/entities/email/email-details.component.ts
@@ -4,6 +4,7 @@ import { mixins } from 'vue-class-component';
 import JhiDataUtils from '@/shared/data/data-utils.service';
 
 import { IEmail } from '@/shared/model/email.model';
+import { MessageStatus } from '@/shared/model/enumerations/message-status.model';
 import EmailService from './email.service';
 
 @Component
@@ -29,5 +30,17 @@ export default class EmailDetails extends mixins(JhiDataUtils) {
 
   public previousState() {
     this.$router.go(-1);
+  }
+
+  public getVariant(status) {
+    if (MessageStatus.IN_QUEUE === status) {
+      return 'info';
+    } else if (MessageStatus.SENT === status) {
+      return 'success';
+    } else if (MessageStatus.FAILED === status) {
+      return 'danger';
+    } else {
+      return 'secondary';
+    }
   }
 }
