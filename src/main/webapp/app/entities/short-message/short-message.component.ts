ebc39d7937ae534375a61eceef162056f2b4c89f
GLinBoy <hojjatabedie@gmail.com>
2021-08-18 16:02:36 +0430

    📦 NEW: Added getVariant function

--- src/main/webapp/app/entities/short-message/short-message.component.ts
+++ src/main/webapp/app/entities/short-message/short-message.component.ts
@@ -3,6 +3,7 @@ import { mixins } from 'vue-class-component';
 import { Component, Vue, Inject } from 'vue-property-decorator';
 import Vue2Filters from 'vue2-filters';
 import { IShortMessage } from '@/shared/model/short-message.model';
+import { MessageStatus } from '@/shared/model/enumerations/message-status.model';
 
 import ShortMessageService from './short-message.service';
 
@@ -126,5 +127,17 @@ export default class ShortMessage extends Vue {
 
   public closeDialog(): void {
     (<any>this.$refs.removeEntity).hide();
+  }
+
+  public getVariant(status) {
+    if (MessageStatus.IN_QUEUE === status) {
+      return 'info';
+    } else if (MessageStatus.SENT === status) {
+      return 'success';
+    } else if (MessageStatus.FAILED === status) {
+      return 'danger';
+    } else {
+      return 'secondary';
+    }
   }
 }
