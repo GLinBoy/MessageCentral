8f296c58523dea47ee17e9cc624436392038dfb5
GLinBoy <hojjatabedie@gmail.com>
2021-08-19 20:50:08 +0430

    📦 NEW: Added getVariant function

--- src/main/webapp/app/entities/short-message/short-message-details.component.ts
+++ src/main/webapp/app/entities/short-message/short-message-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Vue, Inject } from 'vue-property-decorator';
 
 import { IShortMessage } from '@/shared/model/short-message.model';
+import { MessageStatus } from '@/shared/model/enumerations/message-status.model';
 import ShortMessageService from './short-message.service';
 
 @Component
@@ -26,5 +27,17 @@ export default class ShortMessageDetails extends Vue {
 
   public previousState() {
     this.$router.go(-1);
+  }
+
+  public getVariant(status) {
+    if (MessageStatus.IN_QUEUE === status) {
+      return 'info';
+    } else if (MessageStatus.SENT === status) {
+      return 'success';
+    } else if (MessageStatus.FAILED === status) {
+      return 'danger';
+    } else {
+      return 'secondary';
+    }
   }
 }
