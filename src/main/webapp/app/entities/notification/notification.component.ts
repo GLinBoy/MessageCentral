cca457feac9dc50ada4750870f83d1cd006b2a12
GLinBoy <hojjatabedie@gmail.com>
2021-08-19 20:53:21 +0430

    📦 NEW: Added getVariant function

--- src/main/webapp/app/entities/notification/notification.component.ts
+++ src/main/webapp/app/entities/notification/notification.component.ts
@@ -3,6 +3,7 @@ import { mixins } from 'vue-class-component';
 import { Component, Vue, Inject } from 'vue-property-decorator';
 import Vue2Filters from 'vue2-filters';
 import { INotification } from '@/shared/model/notification.model';
+import { MessageStatus } from '@/shared/model/enumerations/message-status.model';
 
 import NotificationService from './notification.service';
 
@@ -126,5 +127,17 @@ export default class Notification extends Vue {
 
   public closeDialog(): void {
     (<any>this.$refs.removeEntity).hide();
+  }
+
+  public getVariant(status) {
+    if (MessageStatus.IN_QUEUE === status) {
+      return 'info';
+    } else if (MessageStatus.SENT === status) {
+      return 'success';
+    } else if (MessageStatus.FAILED === status) {
+      return 'danger';
+    } else {
+      return 'secondary';
+    }
   }
 }
