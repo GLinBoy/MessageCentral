bd5a992793afc049844bdbf4ed09658bf8b4d01c
GLinBoy <hojjatabedie@gmail.com>
2021-09-15 21:21:53 +0430

    📦 NEW: Added multiple SMS model
    resolved #325

--- src/main/webapp/app/shared/model/short-message.model.ts
+++ src/main/webapp/app/shared/model/short-message.model.ts
@@ -9,3 +9,13 @@ export interface IShortMessage {
 export class ShortMessage implements IShortMessage {
   constructor(public id?: number, public phoneNumber?: string, public content?: string, public status?: MessageStatus | null) {}
 }
+
+export interface IShortMessages {
+  phoneNumbers?: string[];
+  content?: string;
+  status?: MessageStatus | null;
+}
+
+export class ShortMessages implements IShortMessages {
+  constructor(public phoneNumbers?: string[], public content?: string, public status?: MessageStatus | null) {}
+}
