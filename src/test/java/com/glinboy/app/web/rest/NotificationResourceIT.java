bd982e4893a9103390800e5120634f6047c2d5de
GLinBoy <hojjatabedie@gmail.com>
2021-09-30 14:29:21 +0330

    🐛 FIX: Test getAllNotificationsBySubjectNotContainsSomething
    #350

--- src/test/java/com/glinboy/app/web/rest/NotificationResourceIT.java
+++ src/test/java/com/glinboy/app/web/rest/NotificationResourceIT.java
@@ -700,10 +700,10 @@ class NotificationResourceIT {
         notificationRepository.saveAndFlush(notification);
 
         // Get all the notificationList where subject does not contain DEFAULT_SUBJECT
-        defaultNotificationShouldNotBeFound("search=subject!*" + DEFAULT_SUBJECT + "*");
+        defaultNotificationShouldNotBeFound("query=subject!=*" + DEFAULT_SUBJECT + "*");
 
         // Get all the notificationList where subject does not contain UPDATED_SUBJECT
-        defaultNotificationShouldBeFound("search=subject!*" + UPDATED_SUBJECT + "*");
+        defaultNotificationShouldBeFound("query=subject!=*" + UPDATED_SUBJECT + "*");
     }
 
     @Test
