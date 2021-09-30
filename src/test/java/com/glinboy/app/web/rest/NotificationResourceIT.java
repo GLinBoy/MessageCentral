74edfc177c3e15aadc40eb330ddfdbea9cebd8b8
GLinBoy <hojjatabedie@gmail.com>
2021-09-30 14:30:04 +0330

    🐛 FIX: Test getAllNotificationsByContentIsEqualToSomething
    #350

--- src/test/java/com/glinboy/app/web/rest/NotificationResourceIT.java
+++ src/test/java/com/glinboy/app/web/rest/NotificationResourceIT.java
@@ -714,10 +714,10 @@ class NotificationResourceIT {
         notificationRepository.saveAndFlush(notification);
 
         // Get all the notificationList where content equals to DEFAULT_CONTENT
-        defaultNotificationShouldBeFound("search=content:" + DEFAULT_CONTENT);
+        defaultNotificationShouldBeFound("query=content==" + DEFAULT_CONTENT);
 
         // Get all the notificationList where content equals to UPDATED_CONTENT
-        defaultNotificationShouldNotBeFound("search=content:" + UPDATED_CONTENT);
+        defaultNotificationShouldNotBeFound("query=content==" + UPDATED_CONTENT);
     }
 
     @Test
