49638d1be4daffe78fe6966af906de1272ed009f
GLinBoy <hojjatabedie@gmail.com>
2021-10-01 11:15:38 +0330

    🐛 FIX: Test getAllNotificationsByImageContainsSomething
    #350

--- src/test/java/com/glinboy/app/web/rest/NotificationResourceIT.java
+++ src/test/java/com/glinboy/app/web/rest/NotificationResourceIT.java
@@ -854,10 +854,10 @@ class NotificationResourceIT {
         notificationRepository.saveAndFlush(notification);
 
         // Get all the notificationList where image contains DEFAULT_IMAGE
-        defaultNotificationShouldBeFound("search=image:*" + DEFAULT_IMAGE + "*");
+        defaultNotificationShouldBeFound("query=image==*" + DEFAULT_IMAGE + "*");
 
         // Get all the notificationList where image contains UPDATED_IMAGE
-        defaultNotificationShouldNotBeFound("search=image:*" + UPDATED_IMAGE + "*");
+        defaultNotificationShouldNotBeFound("query=image==*" + UPDATED_IMAGE + "*");
     }
 
     @Test
