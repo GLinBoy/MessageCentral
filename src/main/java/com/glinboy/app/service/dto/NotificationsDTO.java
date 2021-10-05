d96e250034a49bdca059f1ac774c73ab905e04da
GLinBoy <hojjatabedie@gmail.com>
2021-10-05 15:17:01 +0330

    👌 IMPROVE: Updated receivers from Map to List of ReceiverDTO
    resolved #367

--- src/main/java/com/glinboy/app/service/dto/NotificationsDTO.java
+++ src/main/java/com/glinboy/app/service/dto/NotificationsDTO.java
@@ -1,9 +1,8 @@
 package com.glinboy.app.service.dto;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,105 +10,104 @@ import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.apache.commons.collections.MapUtils;
-
 /**
  * A DTO for the {@link com.glinboy.app.domain.Notification} entity.
  */
 public class NotificationsDTO implements Serializable {
 
-	private Map<String, String> receivers = new HashMap<>();
+    private List<ReceiverDTO> receivers;
 
-	@NotNull
-	@Size(max = 128)
-	private String subject;
+    @NotNull
+    @Size(max = 128)
+    private String subject;
 
-	@NotNull
-	@Size(max = 4000)
-	private String content;
+    @NotNull
+    @Size(max = 4000)
+    private String content;
 
-	@Size(max = 256)
-	private String image;
+    @Size(max = 256)
+    private String image;
 
-	private Set<NotificationDataDTO> data = new HashSet<>();
+    private Set<NotificationDataDTO> data = new HashSet<>();
 
-	public Map<String, String> getReceivers() {
-		return receivers;
-	}
+    public List<ReceiverDTO> getReceivers() {
+        return receivers;
+    }
 
-	public void setReceivers(Map<String, String> receivers) {
-		this.receivers = receivers;
-	}
+    public void setReceivers(List<ReceiverDTO> receivers) {
+        this.receivers = receivers;
+    }
 
-	public String getSubject() {
-		return subject;
-	}
+    public String getSubject() {
+        return subject;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-	public Set<NotificationDataDTO> getData() {
-		return data;
-	}
+    public Set<NotificationDataDTO> getData() {
+        return data;
+    }
 
-	public void setData(Set<NotificationDataDTO> data) {
-		this.data = data;
-	}
+    public void setData(Set<NotificationDataDTO> data) {
+        this.data = data;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (!(o instanceof NotificationsDTO)) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof NotificationsDTO)) {
+            return false;
+        }
 
-		NotificationsDTO notificationsDTO = (NotificationsDTO) o;
+        NotificationsDTO notificationsDTO = (NotificationsDTO) o;
 
-		return !MapUtils.isEmpty(this.receivers) && 
-				this.receivers.size() == notificationsDTO.receivers.size()
-				&& this.receivers.entrySet().containsAll(notificationsDTO.receivers.entrySet())
-				&& (this.subject == null ? notificationsDTO.subject == null : this.subject.equals(notificationsDTO.subject))
-				&& (this.content == null ? notificationsDTO.content == null : this.content.equals(notificationsDTO.content));
-	}
+        return this.receivers != null && this.receivers.isEmpty()
+                && this.receivers.size() == notificationsDTO.receivers.size()
+                && this.receivers.containsAll(notificationsDTO.receivers)
+                && (this.subject == null ? notificationsDTO.subject == null
+                        : this.subject.equals(notificationsDTO.subject))
+                && (this.content == null ? notificationsDTO.content == null
+                        : this.content.equals(notificationsDTO.content));
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.receivers.keySet().stream()
-				.map(key -> key + "=" + getReceivers().get(key))
-				.collect(Collectors.joining(", ", "{", "}")));
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.receivers.stream()
+                .map(r -> String.join(r.getUsername(), r.getToken(), ":"))
+                .collect(Collectors.joining(", ", "{", "}")));
+    }
 
-	// prettier-ignore
-	@Override
-	public String toString() {
-		return "NotificationDTO{" + "receivers=" + getReceivers()
-				.keySet().stream()
-					.map(key -> key + "=" + getReceivers().get(key))
-					.collect(Collectors.joining(", ", "{", "}")) +
-				", subject='" + getSubject() + "'" +
-				", content='" + getContent() + "'" +
-				", image='" + getImage() + "'" +
-				", data='"
-				+ getData().stream().map(NotificationDataDTO::toString)
-				.collect(Collectors.joining(", ")) + "'" + "}";
-	}
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "NotificationDTO{" + "receivers="
+                + getReceivers().stream()
+                    .map(r -> String.join(r.getUsername(), r.getToken(), ":"))
+                    .collect(Collectors.joining(", ", "{", "}"))
+                + ", subject='" + getSubject() + "'" + ", content='" + getContent() + "'" + ", image='" + getImage()
+                + "'" + ", data='"
+                + getData().stream()
+                    .map(NotificationDataDTO::toString)
+                    .collect(Collectors.joining(", ")) + "'" + "}";
+    }
 }
