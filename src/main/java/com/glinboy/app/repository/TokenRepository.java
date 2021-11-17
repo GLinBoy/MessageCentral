5269046fb0d313c3122c4cc939b086b84b8f58ea
GLinBoy <hojjatabedie@gmail.com>
2021-11-17 20:28:33 +0330

    📦 NEW: Added a method to update token status in repository
    resolved #493

--- src/main/java/com/glinboy/app/repository/TokenRepository.java
+++ src/main/java/com/glinboy/app/repository/TokenRepository.java
@@ -2,6 +2,7 @@ package com.glinboy.app.repository;
 
 import com.glinboy.app.domain.Token;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface TokenRepository extends JpaRepository<Token, Long>, JpaSpecificationExecutor<Token> {}
+public interface TokenRepository extends JpaRepository<Token, Long>, JpaSpecificationExecutor<Token> {
+    @Modifying
+    @Query("update token t set t.disable = :disable where t.id = :id")
+    void updateTokenStatus(@Param("id") Long id, @Param("disable") Boolean disable);
+}
