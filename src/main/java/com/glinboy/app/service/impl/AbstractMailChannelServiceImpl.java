41e67171a2f4f704309bce12a425af2521952652
GLinBoy <hojjatabedie@gmail.com>
2022-04-15 16:39:12 +0200

    👌 IMPROVE: Update Mail channel service to publish event

--- src/main/java/com/glinboy/app/service/impl/AbstractMailChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/AbstractMailChannelServiceImpl.java
@@ -1,14 +1,16 @@
 package com.glinboy.app.service.impl;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 
 import com.glinboy.app.config.ApplicationProperties;
-import com.glinboy.app.service.EmailService;
+import com.glinboy.app.event.EmailSentFailedEvent;
+import com.glinboy.app.event.EmailSentSuccessfulEvent;
 import com.glinboy.app.service.MailChannelService;
 import com.glinboy.app.service.dto.EmailDTO;
 
@@ -20,19 +22,17 @@ public abstract class AbstractMailChannelServiceImpl implements MailChannelServi
 
     protected final ApplicationProperties properties;
 
-    protected final JmsTemplate jmsTemplate;
+    private final ApplicationEventPublisher publisher;
 
-    protected AbstractMailChannelServiceImpl(JmsTemplate jmsTemplate,
+    protected AbstractMailChannelServiceImpl(ApplicationEventPublisher publisher,
             ApplicationProperties properties) {
-        this.jmsTemplate = jmsTemplate;
+        this.publisher = publisher;
         this.properties = properties;
     }
 
     @Override
     public void sendMessage(EmailDTO... emailDTOs) {
-        for(var i = 0; i < emailDTOs.length; i++) {
-            jmsTemplate.convertAndSend(TOPIC_NAME, emailDTOs[i]);
-        }
+        publisher.publishEvent(emailDTOs);
     }
 
     @Override
