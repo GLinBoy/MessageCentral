406988a58ce54774d8a6e1a3119d95c9f67cd850
GLinBoy <hojjatabedie@gmail.com>
2022-04-15 16:34:12 +0200

    📦 NEW: Added a class event for successful email send

---
+++ src/main/java/com/glinboy/app/event/EmailSentSuccessfulEvent.java
@@ -0,0 +1,13 @@
+package com.glinboy.app.event;
+
+import java.util.List;
+
+public class EmailSentSuccessfulEvent extends MessageSentSuccessfulEvent {
+
+	private static final long serialVersionUID = 2065902854061173261L;
+
+	public EmailSentSuccessfulEvent(Object source, List<Long> ids) {
+		super(source, ids);
+	}
+
+}
