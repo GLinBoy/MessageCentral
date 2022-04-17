24c66311f3ea85aa8fefe626458dd64805f9d474
GLinBoy <hojjatabedie@gmail.com>
2022-04-17 11:09:49 +0200

    📦 NEW: Added notification sent failed event

---
+++ src/main/java/com/glinboy/app/event/NotificationSentFailedEvent.java
@@ -0,0 +1,13 @@
+package com.glinboy.app.event;
+
+import java.util.List;
+
+public class NotificationSentFailedEvent extends MessageSentSuccessfulEvent {
+
+	private static final long serialVersionUID = 2065902854061173261L;
+
+	public NotificationSentFailedEvent(Object source, List<Long> ids) {
+		super(source, ids);
+	}
+
+}
