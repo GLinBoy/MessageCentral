67151c2f656ee1d6081238c0ba159909aae88fa2
GLinBoy <hojjatabedie@gmail.com>
2022-07-05 20:38:34 +0200

    👌 IMPROVE: Added /api/dashboard API

--- src/main/java/com/glinboy/app/web/rest/DashboardResource.java
+++ src/main/java/com/glinboy/app/web/rest/DashboardResource.java
@@ -2,6 +2,8 @@ package com.glinboy.app.web.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +16,9 @@ public class DashboardResource {
 
     private final Logger log = LoggerFactory.getLogger(DashboardResource.class);
 
+    @GetMapping("/dashboard")
+    public ResponseEntity<String> getDashboard() {
+        log.info("Dashboard has been called!");
+        return ResponseEntity.ok("Dashboard");
+    }
 }
