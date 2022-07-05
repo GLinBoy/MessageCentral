ebaa5d8c5d39f07f9ab4a68b91d95bbef5b32602
GLinBoy <hojjatabedie@gmail.com>
2022-07-05 20:22:05 +0200

    👌 IMPROVE: Enabled rest controller for DashboardResource

--- src/main/java/com/glinboy/app/web/rest/DashboardResource.java
+++ src/main/java/com/glinboy/app/web/rest/DashboardResource.java
@@ -1,4 +1,12 @@
 package com.glinboy.app.web.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for managing Dashboard.
+ */
+@RestController
+@RequestMapping("/api")
 public class DashboardResource {
 }
