a3e3cf1922f3415532b061fe7184673d4cd4e708
GLinBoy <hojjatabedie@gmail.com>
2022-07-06 21:31:35 +0200

    👌 IMPROVE: Reformat code util package

--- src/main/java/com/glinboy/app/util/Patterns.java
+++ src/main/java/com/glinboy/app/util/Patterns.java
@@ -1,11 +1,11 @@
 package com.glinboy.app.util;
 
 public final class Patterns {
-	
-	private Patterns() {}
-	
-	public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-	
-	public static final String SMS_PATTERN = "^\\+(?:[0-9]●?){6,14}[0-9]$";
+
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    public static final String SMS_PATTERN = "^\\+(?:[0-9]●?){6,14}[0-9]$";
+
+    private Patterns() {
+    }
 
 }
