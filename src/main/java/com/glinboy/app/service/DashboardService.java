74a2b8d1916a8f5c70ea8b938539a8b7435139f1
GLinBoy <hojjatabedie@gmail.com>
2022-07-09 19:21:21 +0200

    👌 IMPROVE: Added getLast30DaysMessageStatics to MessagesStatic projection

--- src/main/java/com/glinboy/app/service/DashboardService.java
+++ src/main/java/com/glinboy/app/service/DashboardService.java
@@ -1,4 +1,9 @@
 package com.glinboy.app.service;
 
+import com.glinboy.app.domain.projection.MessagesStatic;
+
+import java.util.List;
+
 public interface DashboardService {
+    List<MessagesStatic> getLast30DaysMessageStatics();
 }
