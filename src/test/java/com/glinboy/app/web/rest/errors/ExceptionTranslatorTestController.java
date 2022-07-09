8b4bf55cf93710a15cef669c5503804afb600169
GLinBoy <hojjatabedie@gmail.com>
2022-07-09 13:27:26 +0200

    👌 IMPROVE: Reformatted web.rest.error package test

--- src/test/java/com/glinboy/app/web/rest/errors/ExceptionTranslatorTestController.java
+++ src/test/java/com/glinboy/app/web/rest/errors/ExceptionTranslatorTestController.java
@@ -1,12 +1,13 @@
 package com.glinboy.app.web.rest.errors;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import org.springframework.dao.ConcurrencyFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/api/exception-translator-test")
@@ -18,13 +19,16 @@ public class ExceptionTranslatorTestController {
     }
 
     @PostMapping("/method-argument")
-    public void methodArgument(@Valid @RequestBody TestDTO testDTO) {}
+    public void methodArgument(@Valid @RequestBody TestDTO testDTO) {
+    }
 
     @GetMapping("/missing-servlet-request-part")
-    public void missingServletRequestPartException(@RequestPart String part) {}
+    public void missingServletRequestPartException(@RequestPart String part) {
+    }
 
     @GetMapping("/missing-servlet-request-parameter")
-    public void missingServletRequestParameterException(@RequestParam String param) {}
+    public void missingServletRequestParameterException(@RequestParam String param) {
+    }
 
     @GetMapping("/access-denied")
     public void accessdenied() {
@@ -62,5 +66,6 @@ public class ExceptionTranslatorTestController {
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "test response status")
     @SuppressWarnings("serial")
-    public static class TestResponseStatusException extends RuntimeException {}
+    public static class TestResponseStatusException extends RuntimeException {
+    }
 }
