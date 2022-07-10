c9e722c8ed10cf00e02cbdd96545e0a76f68c8e2
GLinBoy <hojjatabedie@gmail.com>
2022-07-10 13:36:29 +0200

    👌 IMPROVE: Rename method to getLast30DaysMessageStatics in service impl.

--- src/main/java/com/glinboy/app/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/DashboardServiceImpl.java
@@ -1,10 +1,17 @@
 package com.glinboy.app.service.impl;
 
+import com.glinboy.app.domain.MessagesStatics;
 import com.glinboy.app.service.DashboardService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 public class DashboardServiceImpl implements DashboardService {
+    @Override
+    public List<MessagesStatics> getLast30DaysMessagesStatics() {
+        return List.of();
+    }
 }
