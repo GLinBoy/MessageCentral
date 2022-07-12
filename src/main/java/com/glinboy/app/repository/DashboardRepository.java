aec23cc06561bc3784897567afe271ceb86dafb4
GLinBoy <hojjatabedie@gmail.com>
2022-07-12 20:20:44 +0200

    👌 IMPROVE: Added DashboardRepository API

--- src/main/java/com/glinboy/app/repository/DashboardRepository.java
+++ src/main/java/com/glinboy/app/repository/DashboardRepository.java
@@ -1,4 +1,9 @@
 package com.glinboy.app.repository;
 
+import com.glinboy.app.domain.MessagesStatics;
+
+import java.util.List;
+
 public interface DashboardRepository {
+    List<MessagesStatics> getLast30DaysMessagesStatics();
 }
