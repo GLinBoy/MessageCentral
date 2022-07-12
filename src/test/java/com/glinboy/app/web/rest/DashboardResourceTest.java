e71175413178ee1c4ff431ba66997e71e2a3232c
GLinBoy <hojjatabedie@gmail.com>
2022-07-12 20:38:40 +0200

    👌 IMPROVE: Added DashboardResourceTest

---
+++ src/test/java/com/glinboy/app/web/rest/DashboardResourceTest.java
@@ -0,0 +1,12 @@
+package com.glinboy.app.web.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DashboardResourceTest {
+
+    @Test
+    void getDashboard() {
+    }
+}
