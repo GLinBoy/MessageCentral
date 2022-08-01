d9d9dc995aa92a5026b8eb8ee10337214c5d6112
GLinBoy <hojjatabedie@gmail.com>
2022-08-01 20:59:56 +0200

    📦 NEW: Added materialized view query of messages_statistics_vw

--- src/main/java/com/glinboy/app/repository/impl/DashboardRepositoryImpl.java
+++ src/main/java/com/glinboy/app/repository/impl/DashboardRepositoryImpl.java
@@ -1,7 +1,8 @@
 package com.glinboy.app.repository.impl;
 
-import com.glinboy.app.domain.MessagesStatics;
+import com.glinboy.app.domain.MessagesStatistics;
 import com.glinboy.app.repository.DashboardRepository;
+import com.glinboy.app.repository.rowmapper.MessagesStatisticsRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,12 +13,21 @@ public class DashboardRepositoryImpl implements DashboardRepository {
 
     private final JdbcTemplate template;
 
+    private static final String LAST_30_DAYS_MESSAGES_STATISTICS_QUERY = " SELECT msv.date, " +
+        "        msv.total_email_sent, " +
+        "        msv.total_email_failed, " +
+        "        msv.total_sms_sent, " +
+        "        msv.total_sms_failed, " +
+        "        msv.total_notification_sent, " +
+        "        msv.total_notification_failed " +
+        " FROM messages_statistics_vw msv ";
+
     public DashboardRepositoryImpl(JdbcTemplate template) {
         this.template = template;
     }
 
     @Override
-    public List<MessagesStatics> getLast30DaysMessagesStatics() {
-        return List.of();
+    public List<MessagesStatistics> getLast30DaysMessagesStatics() {
+        return template.query(LAST_30_DAYS_MESSAGES_STATISTICS_QUERY, new MessagesStatisticsRowMapper());
     }
 }
---
+++ src/main/resources/config/liquibase/sql/messages_statistics_materialized_view_postgresql.sql
@@ -0,0 +1,28 @@
+CREATE MATERIALIZED VIEW messages_statistics_vw
+AS
+    SELECT
+        s.date,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'EMAIL' AND s.status = 'SENT'), 0) AS total_email_sent,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'EMAIL' AND s.status = 'FAILED'), 0) AS total_email_failed,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'EMAIL' AND s.status = 'IN_QUEUE'), 0) AS total_email_queue,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'SMS' AND s.status = 'SENT'), 0) AS total_sms_sent,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'SMS' AND s.status = 'FAILED'), 0) AS total_sms_failed,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'SMS' AND s.status = 'IN_QUEUE'), 0) AS total_sms_queue,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'NOTIFICATION' AND s.status = 'SENT'), 0) AS total_notification_sent,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'NOTIFICATION' AND s.status = 'FAILED'), 0) AS total_notification_failed,
+        COALESCE(SUM(s.total) FILTER(WHERE s.type = 'NOTIFICATION' AND s.status = 'IN_QUEUE'), 0) AS total_notification_queue
+    FROM (SELECT CAST(created_at AS DATE) AS date, status, count(*) AS total, 'EMAIL' AS type
+          FROM email
+          GROUP BY CAST(created_at AS DATE), status
+          UNION ALL
+          SELECT CAST(created_at AS DATE) AS date, status, count(*) AS total, 'SMS' AS type
+          FROM short_message
+          GROUP BY CAST(created_at AS DATE), status
+          UNION ALL
+          SELECT CAST(created_at AS DATE) AS date, status, count(*) AS total, 'NOTIFICATION' AS type
+          FROM notification
+          GROUP BY CAST(created_at AS DATE), status) s
+    WHERE s.date < CURRENT_DATE AND s.date >= CURRENT_DATE - 30
+    GROUP BY s.date
+    ORDER BY s.date DESC
+WITH DATA;
