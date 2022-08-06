ee5dca11b21d30f9d8a4a353a351b46e71d0bd88
GLinBoy <hojjatabedie@gmail.com>
2022-08-06 21:43:55 +0200

    🐛 FIX: Updated testRegisterAdminIsIgnored method

--- src/test/java/com/glinboy/app/web/rest/AccountResourceIT.java
+++ src/test/java/com/glinboy/app/web/rest/AccountResourceIT.java
@@ -370,7 +370,7 @@ class AccountResourceIT {
 
         restAccountMockMvc
             .perform(post("/api/register").contentType(MediaType.APPLICATION_JSON).content(TestUtil.convertObjectToJsonBytes(validUser)))
-            .andExpect(status().isMethodNotAllowed());
+            .andExpect(status().isNotFound());
     }
 
     @Test
