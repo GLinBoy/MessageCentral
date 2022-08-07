e8a6ad6506be24c08b56f08febcce4b920d084cc
GLinBoy <hojjatabedie@gmail.com>
2022-08-07 18:28:09 +0200

    📦 NEW: added a method to retrieve 30 days to the dashboard service

--- src/main/webapp/app/core/dashboard/dashboard.service.ts
+++ src/main/webapp/app/core/dashboard/dashboard.service.ts
@@ -1,1 +1,18 @@
-export default class DashboardService {}
+import axios from 'axios';
+
+const baseApiUrl = 'api/dashboard';
+
+export default class DashboardService {
+  public retrieve30Days(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      axios
+        .get(baseApiUrl)
+        .then(res => {
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+}
