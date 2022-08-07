d6a5e862d67bb9cf282ea2fc38125a72d89e2181
GLinBoy <hojjatabedie@gmail.com>
2022-08-07 19:16:17 +0200

    🐛 FIX: Renamed Home to Dashboard in dashboard component

--- src/main/webapp/app/core/dashboard/dashboard.component.ts
+++ src/main/webapp/app/core/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import Component from 'vue-class-component';
 import { Inject, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Home extends Vue {
+export default class Dashboard extends Vue {
+  @Inject('dashboardService') private dashboardService: () => DashboardService;
+
   public get authenticated(): boolean {
     return this.$store.getters.authenticated;
   }
