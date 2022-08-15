72ba219cd6ff2da71fec02bc721acd568f6d3b8c
GLinBoy <hojjatabedie@gmail.com>
2022-08-15 21:04:16 +0200

    📦 NEW: Added MessagesStatistics class

--- src/main/webapp/app/shared/model/messages-statistics.model.ts
+++ src/main/webapp/app/shared/model/messages-statistics.model.ts
@@ -6,3 +6,7 @@ export interface IMessagesStatistics {
   sms?: Statistics;
   notification?: Statistics;
 }
+
+export class MessagesStatistics implements IMessagesStatistics {
+  constructor(public date?: Date, public email?: Statistics, public sms?: Statistics, public notification?: Statistics) {}
+}
