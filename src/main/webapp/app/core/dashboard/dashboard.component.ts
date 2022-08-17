da1462d0f1a36b2c4d3d70425382c657cea8b77c
GLinBoy <hojjatabedie@gmail.com>
2022-08-17 19:54:55 +0200

    🐛 FIX: Added missed AlertService

--- src/main/webapp/app/core/dashboard/dashboard.component.ts
+++ src/main/webapp/app/core/dashboard/dashboard.component.ts
@@ -1,26 +1,38 @@
 import { mixins } from 'vue-class-component';
 import { Component, Inject } from 'vue-property-decorator';
+import { IMessagesStatistics } from '@/shared/model/messages-statistics.model';
 
 import DashboardService from './dashboard.service';
 import JhiDataUtils from '@/shared/data/data-utils.service';
+import AlertService from '@/shared/alert/alert.service';
 
 @Component
 export default class Dashboard extends mixins(JhiDataUtils) {
   @Inject('dashboardService') private dashboardService: () => DashboardService;
+  @Inject('alertService') private alertService: () => AlertService;
+
+  public messagesStatistics: IMessagesStatistics[] = [];
+
+  public isFetching = false;
 
   public mounted(): void {
     this.retrieveLast30DaysMessagesStatistic();
   }
 
   public retrieveLast30DaysMessagesStatistic(): void {
+    this.isFetching = true;
     this.dashboardService()
       .retrieveLast30DaysMessagesStatistic()
       .then(
         res => {
-          console.log(res);
+          this.messagesStatistics = res.data;
+          console.log(this.messagesStatistics);
+          this.isFetching = false;
         },
         err => {
+          this.isFetching = false;
           console.error(err);
+          this.alertService().showHttpError(this, err.response);
         }
       );
   }
