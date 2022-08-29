8cdd120155785ebf9835a9811536e261b18f3cf0
GLinBoy <hojjatabedie@gmail.com>
2022-08-29 20:19:16 +0200

    👌 IMPROVE: Added cards for statistics

--- src/main/webapp/app/core/dashboard/dashboard.vue
+++ src/main/webapp/app/core/dashboard/dashboard.vue
@@ -4,7 +4,70 @@
       <span class="hipster img-fluid rounded"></span>
     </div>
     <div class="col-md-9">
-      <h1>Dashboard</h1>
+      <b-row class="row">
+        <div class="col-12">
+          <h1>Dashboard</h1>
+        </div>
+      </b-row>
+      <b-row>
+        <b-col sm="3" md="3">
+          <b-card
+            border-variant="primary"
+            header="Total Messages"
+            header-bg-variant="primary"
+            header-text-variant="white"
+            align="center"
+            class="mb-2"
+          >
+            <b-card-text>Sent: 123,456</b-card-text>
+            <b-card-text>Failed: 789</b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="12" md="3">
+          <b-card
+            border-variant="primary"
+            header="Emails"
+            header-bg-variant="primary"
+            header-text-variant="white"
+            align="center"
+            class="mb-2"
+          >
+            <b-card-text>Sent: 123,456</b-card-text>
+            <b-card-text>Failed: 789</b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="12" md="3">
+          <b-card
+            border-variant="primary"
+            header="SMSs"
+            header-bg-variant="primary"
+            header-text-variant="white"
+            align="center"
+            class="mb-2"
+          >
+            <b-card-text>Sent: 123,456</b-card-text>
+            <b-card-text>Failed: 789</b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="12" md="3">
+          <b-card
+            border-variant="primary"
+            header="Notifications"
+            header-bg-variant="primary"
+            header-text-variant="white"
+            align="center"
+            class="mb-2"
+          >
+            <b-card-text>Sent: 123,456</b-card-text>
+            <b-card-text>Failed: 789</b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+      <div class="row">
+        <div class="col-12">
+          <h3>status</h3>
+        </div>
+      </div>
     </div>
   </div>
 </template>
