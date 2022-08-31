b82986a7e793ece7c7ba038865985239296881fc
GLinBoy <hojjatabedie@gmail.com>
2022-08-31 20:10:04 +0200

    👌 IMPROVE: Used sms statistics computed variable

--- src/main/webapp/app/core/dashboard/dashboard.vue
+++ src/main/webapp/app/core/dashboard/dashboard.vue
@@ -45,8 +45,8 @@
             align="center"
             class="mb-2"
           >
-            <b-card-text>Sent: 123,456</b-card-text>
-            <b-card-text>Failed: 789</b-card-text>
+            <b-card-text>Sent: {{ smsSuccessfulSent }}</b-card-text>
+            <b-card-text>Failed: {{ smsFailedSent }}</b-card-text>
           </b-card>
         </b-col>
         <b-col sm="12" md="3">
