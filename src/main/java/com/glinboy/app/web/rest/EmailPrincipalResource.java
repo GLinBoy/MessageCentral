1826ce9b3024a4a57186e6d00a19339661676fb2
GLinBoy <hojjatabedie@gmail.com>
2022-11-20 19:26:52 +0100

    👌 IMPROVE: Moved business to the resource principal of Email

--- src/main/java/com/glinboy/app/web/rest/EmailPrincipalResource.java
+++ src/main/java/com/glinboy/app/web/rest/EmailPrincipalResource.java
@@ -1,16 +1,225 @@
 package com.glinboy.app.web.rest;
 
+import com.glinboy.app.domain.Email;
 import com.glinboy.app.repository.EmailRepository;
+import com.glinboy.app.rsql.CustomRsqlVisitor;
 import com.glinboy.app.service.EmailQueryService;
 import com.glinboy.app.service.EmailService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.glinboy.app.service.dto.EmailDTO;
+import com.glinboy.app.service.dto.EmailsDTO;
+import com.glinboy.app.web.rest.errors.BadRequestAlertException;
+import cz.jirutka.rsql.parser.RSQLParser;
+import cz.jirutka.rsql.parser.ast.Node;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tech.jhipster.web.util.HeaderUtil;
+import tech.jhipster.web.util.PaginationUtil;
+import tech.jhipster.web.util.ResponseUtil;
 
 @RestController
 @RequestMapping("/api")
 public class EmailPrincipalResource extends EmailResource {
 
+    private static final String ENTITY_NAME = "email";
+    private final Logger log = LoggerFactory.getLogger(EmailResource.class);
+    private final EmailService emailService;
+    private final EmailRepository emailRepository;
+    private final EmailQueryService emailQueryService;
+
+    @Value("${jhipster.clientApp.name}")
+    private String applicationName;
+
     public EmailPrincipalResource(EmailService emailService, EmailRepository emailRepository, EmailQueryService emailQueryService) {
-        super(emailService, emailRepository, emailQueryService);
+        this.emailService = emailService;
+        this.emailRepository = emailRepository;
+        this.emailQueryService = emailQueryService;
+    }
+
+    /**
+     * {@code POST  /emails} : Create a new email.
+     *
+     * @param emailDTO the emailDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new emailDTO, or with status {@code 400 (Bad Request)} if the email has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("/emails")
+    public ResponseEntity<EmailDTO> createEmail(@Valid @RequestBody EmailDTO emailDTO) throws URISyntaxException {
+        log.debug("REST request to save Email : {}", emailDTO);
+        if (emailDTO.getId() != null) {
+            throw new BadRequestAlertException("A new email cannot already have an ID", ENTITY_NAME, "idexists");
+        }
+        EmailDTO result = emailService.save(emailDTO);
+        return ResponseEntity
+            .created(new URI("/api/emails/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code POST  /emails} : Create a new email.
+     *
+     * @param emailDTO the emailDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new emailDTO, or with status {@code 400 (Bad Request)} if the email has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    @PostMapping("/emails/multiple")
+    public ResponseEntity<Void> createEmails(@Valid @RequestBody List<EmailsDTO> emailsDTO) throws URISyntaxException {
+        log.debug("REST request to save Emails : {}", emailsDTO.stream().map(EmailsDTO::toString).collect(Collectors.joining(",\n")));
+        emailService.save(emailsDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * {@code PUT  /emails/:id} : Updates an existing email.
+     *
+     * @param id       the id of the emailDTO to save.
+     * @param emailDTO the emailDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated emailDTO,
+     * or with status {@code 400 (Bad Request)} if the emailDTO is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the emailDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    //    @PutMapping("/emails/{id}")
+    public ResponseEntity<EmailDTO> updateEmail(
+        @PathVariable(value = "id", required = false) final Long id,
+        @Valid @RequestBody EmailDTO emailDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to update Email : {}, {}", id, emailDTO);
+        if (emailDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, emailDTO.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!emailRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        EmailDTO result = emailService.update(emailDTO);
+        return ResponseEntity
+            .ok()
+            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, emailDTO.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * {@code PATCH  /emails/:id} : Partial updates given fields of an existing email, field will ignore if it is null
+     *
+     * @param id       the id of the emailDTO to save.
+     * @param emailDTO the emailDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated emailDTO,
+     * or with status {@code 400 (Bad Request)} if the emailDTO is not valid,
+     * or with status {@code 404 (Not Found)} if the emailDTO is not found,
+     * or with status {@code 500 (Internal Server Error)} if the emailDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
+    //    @PatchMapping(value = "/emails/{id}", consumes = "application/merge-patch+json")
+    public ResponseEntity<EmailDTO> partialUpdateEmail(
+        @PathVariable(value = "id", required = false) final Long id,
+        @NotNull @RequestBody EmailDTO emailDTO
+    ) throws URISyntaxException {
+        log.debug("REST request to partial update Email partially : {}, {}", id, emailDTO);
+        if (emailDTO.getId() == null) {
+            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+        }
+        if (!Objects.equals(id, emailDTO.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!emailRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+
+        Optional<EmailDTO> result = emailService.partialUpdate(emailDTO);
+
+        return ResponseUtil.wrapOrNotFound(
+            result,
+            HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, emailDTO.getId().toString())
+        );
+    }
+
+    /**
+     * {@code GET  /emails} : get all the emails.
+     *
+     * @param pageable the pagination information.
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of emails in body.
+     */
+    @GetMapping("/emails")
+    public ResponseEntity<List<EmailDTO>> getAllEmails(
+        @org.springdoc.api.annotations.ParameterObject Pageable pageable,
+        @RequestParam(value = "query", required = false, defaultValue = "") String query
+    ) {
+        Specification<Email> specs = Specification.where(null);
+        if (!StringUtils.isBlank(query)) {
+            Node rootNode = new RSQLParser().parse(query);
+            specs = rootNode.accept(new CustomRsqlVisitor<Email>());
+        }
+        Page<EmailDTO> page = emailQueryService.findBySearch(specs, pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+        return ResponseEntity.ok().headers(headers).body(page.getContent());
+    }
+
+    /**
+     * {@code GET  /emails/count} : count all the emails.
+     *
+     * @param criteria the criteria which the requested entities should match.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the count in body.
+     */
+    @GetMapping("/emails/count")
+    public ResponseEntity<Long> countEmails(@RequestParam(value = "query", required = false, defaultValue = "") String query) {
+        Specification<Email> specs = Specification.where(null);
+        if (!StringUtils.isBlank(query)) {
+            Node rootNode = new RSQLParser().parse(query);
+            specs = rootNode.accept(new CustomRsqlVisitor<Email>());
+        }
+        return ResponseEntity.ok().body(emailQueryService.countBySpecification(specs));
+    }
+
+    /**
+     * {@code GET  /emails/:id} : get the "id" email.
+     *
+     * @param id the id of the emailDTO to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the emailDTO, or with status {@code 404 (Not Found)}.
+     */
+    @GetMapping("/emails/{id}")
+    public ResponseEntity<EmailDTO> getEmail(@PathVariable Long id) {
+        log.debug("REST request to get Email : {}", id);
+        Optional<EmailDTO> emailDTO = emailService.findOne(id);
+        return ResponseUtil.wrapOrNotFound(emailDTO);
+    }
+
+    /**
+     * {@code DELETE  /emails/:id} : delete the "id" email.
+     *
+     * @param id the id of the emailDTO to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
+    //    @DeleteMapping("/emails/{id}")
+    public ResponseEntity<Void> deleteEmail(@PathVariable Long id) {
+        log.debug("REST request to delete Email : {}", id);
+        emailService.delete(id);
+        return ResponseEntity
+            .noContent()
+            .headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+            .build();
     }
 }
