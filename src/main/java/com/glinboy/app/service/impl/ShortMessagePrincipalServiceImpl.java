12362220d2faaac66f883ba8e19b4d68453d10dd
GLinBoy <hojjatabedie@gmail.com>
2022-11-21 12:08:01 +0100

    👌 IMPROVE: Copied business to SMS principal service

--- src/main/java/com/glinboy/app/service/impl/ShortMessagePrincipalServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/ShortMessagePrincipalServiceImpl.java
@@ -1,10 +1,25 @@
 package com.glinboy.app.service.impl;
 
+import com.glinboy.app.domain.ShortMessage;
+import com.glinboy.app.domain.enumeration.MessageStatus;
+import com.glinboy.app.event.ShortMessageSentFailedEvent;
+import com.glinboy.app.event.ShortMessageSentSuccessfulEvent;
 import com.glinboy.app.repository.ShortMessageRepository;
 import com.glinboy.app.service.ShortMessageChannelService;
 import com.glinboy.app.service.dto.ShortMessageDTO;
+import com.glinboy.app.service.dto.ShortMessagesDTO;
 import com.glinboy.app.service.mapper.ShortMessageMapper;
+import com.glinboy.app.util.Patterns;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +28,116 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ShortMessagePrincipalServiceImpl extends ShortMessageServiceImpl {
 
+    private final Logger log = LoggerFactory.getLogger(ShortMessagePrincipalServiceImpl.class);
+
+    private final ShortMessageRepository shortMessageRepository;
+
+    private final ShortMessageMapper shortMessageMapper;
+
+    private final ShortMessageChannelService<ShortMessageDTO> smsProvider;
+
     public ShortMessagePrincipalServiceImpl(
         ShortMessageRepository shortMessageRepository,
         ShortMessageMapper shortMessageMapper,
         ShortMessageChannelService<ShortMessageDTO> smsProvider
     ) {
         super(shortMessageRepository, shortMessageMapper, smsProvider);
+        this.shortMessageRepository = shortMessageRepository;
+        this.shortMessageMapper = shortMessageMapper;
+        this.smsProvider = smsProvider;
+    }
+
+    @Override
+    public ShortMessageDTO save(ShortMessageDTO shortMessageDTO) {
+        log.debug("Request to save ShortMessage : {}", shortMessageDTO);
+        ShortMessage shortMessage = shortMessageMapper.toEntity(shortMessageDTO);
+        if (shortMessage.getId() == null) {
+            shortMessage.setStatus(MessageStatus.IN_QUEUE);
+        }
+        shortMessage = shortMessageRepository.save(shortMessage);
+        ShortMessageDTO smsDTO = shortMessageMapper.toDto(shortMessage);
+        smsProvider.sendMessage(smsDTO);
+        return smsDTO;
+    }
+
+    @Override
+    public List<ShortMessageDTO> save(List<ShortMessagesDTO> shortMessagesDTO) {
+        log.debug("Request to save ShortMessage : {}", shortMessagesDTO);
+        List<ShortMessage> messages = shortMessagesDTO
+            .stream()
+            .flatMap(ss ->
+                Set
+                    .copyOf(ss.getPhoneNumbers())
+                    .stream()
+                    .filter(p -> p.matches(Patterns.SMS_PATTERN))
+                    .map(p -> {
+                        ShortMessage s = new ShortMessage();
+                        s.phoneNumber(p);
+                        s.setContent(ss.getContent());
+                        s.setStatus(MessageStatus.IN_QUEUE);
+                        return s;
+                    })
+            )
+            .collect(Collectors.toList());
+        log.info("List of {} SMSs: {}", messages.size(), messages);
+        messages = this.shortMessageRepository.saveAll(messages);
+        List<ShortMessageDTO> dtoList = this.shortMessageMapper.toDto(messages);
+        this.smsProvider.sendMessage(dtoList.toArray(new ShortMessageDTO[dtoList.size()]));
+        return dtoList;
+    }
+
+    @Override
+    public ShortMessageDTO update(ShortMessageDTO shortMessageDTO) {
+        log.debug("Request to update ShortMessage : {}", shortMessageDTO);
+        ShortMessage shortMessage = shortMessageMapper.toEntity(shortMessageDTO);
+        shortMessage = shortMessageRepository.save(shortMessage);
+        return shortMessageMapper.toDto(shortMessage);
+    }
+
+    @Override
+    public Optional<ShortMessageDTO> partialUpdate(ShortMessageDTO shortMessageDTO) {
+        log.debug("Request to partially update ShortMessage : {}", shortMessageDTO);
+
+        return shortMessageRepository
+            .findById(shortMessageDTO.getId())
+            .map(existingShortMessage -> {
+                shortMessageMapper.partialUpdate(existingShortMessage, shortMessageDTO);
+
+                return existingShortMessage;
+            })
+            .map(shortMessageRepository::save)
+            .map(shortMessageMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<ShortMessageDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all ShortMessages");
+        return shortMessageRepository.findAll(pageable).map(shortMessageMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<ShortMessageDTO> findOne(Long id) {
+        log.debug("Request to get ShortMessage : {}", id);
+        return shortMessageRepository.findById(id).map(shortMessageMapper::toDto);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete ShortMessage : {}", id);
+        shortMessageRepository.deleteById(id);
+    }
+
+    @Transactional
+    @EventListener
+    public void onMessageSent(ShortMessageSentSuccessfulEvent event) {
+        this.shortMessageRepository.updateStatus(MessageStatus.SENT, event.getIds().toArray(new Long[event.getIds().size()]));
+    }
+
+    @Transactional
+    @EventListener
+    public void onMessageFailed(ShortMessageSentFailedEvent event) {
+        this.shortMessageRepository.updateStatus(MessageStatus.FAILED, event.getIds().toArray(new Long[event.getIds().size()]));
     }
 }
