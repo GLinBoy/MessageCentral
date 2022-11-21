ddc9cfab1caddca468859e8834452e1effcf75ac
GLinBoy <hojjatabedie@gmail.com>
2022-11-21 13:42:19 +0100

    👌 IMPROVE: Removed duplicate methods from SMS principal service

--- src/main/java/com/glinboy/app/service/impl/ShortMessagePrincipalServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/ShortMessagePrincipalServiceImpl.java
@@ -11,15 +11,12 @@ import com.glinboy.app.service.dto.ShortMessagesDTO;
 import com.glinboy.app.service.mapper.ShortMessageMapper;
 import com.glinboy.app.util.Patterns;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -84,49 +81,6 @@ public class ShortMessagePrincipalServiceImpl extends ShortMessageServiceImpl {
         List<ShortMessageDTO> dtoList = this.shortMessageMapper.toDto(messages);
         this.smsProvider.sendMessage(dtoList.toArray(new ShortMessageDTO[dtoList.size()]));
         return dtoList;
-    }
-
-    @Override
-    public ShortMessageDTO update(ShortMessageDTO shortMessageDTO) {
-        log.debug("Request to update ShortMessage : {}", shortMessageDTO);
-        ShortMessage shortMessage = shortMessageMapper.toEntity(shortMessageDTO);
-        shortMessage = shortMessageRepository.save(shortMessage);
-        return shortMessageMapper.toDto(shortMessage);
-    }
-
-    @Override
-    public Optional<ShortMessageDTO> partialUpdate(ShortMessageDTO shortMessageDTO) {
-        log.debug("Request to partially update ShortMessage : {}", shortMessageDTO);
-
-        return shortMessageRepository
-            .findById(shortMessageDTO.getId())
-            .map(existingShortMessage -> {
-                shortMessageMapper.partialUpdate(existingShortMessage, shortMessageDTO);
-
-                return existingShortMessage;
-            })
-            .map(shortMessageRepository::save)
-            .map(shortMessageMapper::toDto);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Page<ShortMessageDTO> findAll(Pageable pageable) {
-        log.debug("Request to get all ShortMessages");
-        return shortMessageRepository.findAll(pageable).map(shortMessageMapper::toDto);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Optional<ShortMessageDTO> findOne(Long id) {
-        log.debug("Request to get ShortMessage : {}", id);
-        return shortMessageRepository.findById(id).map(shortMessageMapper::toDto);
-    }
-
-    @Override
-    public void delete(Long id) {
-        log.debug("Request to delete ShortMessage : {}", id);
-        shortMessageRepository.deleteById(id);
     }
 
     @Transactional
