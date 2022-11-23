8938fd853a45be1b21baaea54dd15a1bbe8d413c
GLinBoy <hojjatabedie@gmail.com>
2022-11-23 10:55:46 +0100

    👌 IMPROVE: Update successful/failed messages of toast on the notification multiple page
    resolved #697

--- src/main/webapp/app/entities/notification/notification-multiple.component.ts
+++ src/main/webapp/app/entities/notification/notification-multiple.component.ts
@@ -56,10 +56,10 @@ export default class NotificationMultiple extends Vue {
     this.isSaving = true;
     this.notificationService()
       .createMultiple([this.notifications])
-      .then(param => {
+      .then(() => {
         this.isSaving = false;
         this.$router.go(-1);
-        const message = this.$t('messageCentralApp.notification.created', { param: param.id });
+        const message = this.$t('messageCentralApp.notification.createdMultiple');
         return (this.$root as any).$bvToast.toast(message.toString(), {
           toaster: 'b-toaster-top-center',
           title: 'Success',
