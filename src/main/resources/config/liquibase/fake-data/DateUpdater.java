5b81a8307f6552d16d6fbda571bb79fde088f4e6
GLinBoy <hojjatabedie@gmail.com>
2022-12-11 15:39:34 +0100

    👌 IMPROVE: Renamed file to follow Java code style

--- src/main/resources/config/liquibase/fake-data/DateUpdater.java
+++ src/main/resources/config/liquibase/fake-data/DateUpdater.java
@@ -1,25 +1,31 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.6.3
+//DEPS ch.qos.reload4j:reload4j:1.2.24
 
 import java.util.concurrent.Callable;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "date_updater", mixinStandardHelpOptions = true, version = "date_updater 0.1", description = "date_updater made with jbang")
-class date_updater implements Callable<Integer> {
+@Command(name = "DateUpdater", mixinStandardHelpOptions = true, version = "DateUpdater 0.1", description = "DateUpdater made with jbang")
+class DateUpdater implements Callable<Integer> {
+
+    static final Logger logger = Logger.getLogger(DateUpdater.class);
 
     @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
     private String greeting;
 
     public static void main(String... args) {
-        int exitCode = new CommandLine(new date_updater()).execute(args);
+        BasicConfigurator.configure();
+        int exitCode = new CommandLine(new DateUpdater()).execute(args);
         System.exit(exitCode);
     }
 
     @Override
     public Integer call() throws Exception { // your business logic goes here...
-        System.out.println("Hello " + greeting);
+        logger.info("Hello " + greeting);
         return 0;
     }
 }
