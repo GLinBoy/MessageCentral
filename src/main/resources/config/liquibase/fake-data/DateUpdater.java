15eb1bb70ba5f30d8a22b298e741f9027e428bae
GLinBoy <hojjatabedie@gmail.com>
2022-12-21 18:02:53 +0100

    👌 IMPROVE: Update all months of date to the current month
    resolved #705

--- src/main/resources/config/liquibase/fake-data/DateUpdater.java
+++ src/main/resources/config/liquibase/fake-data/DateUpdater.java
@@ -1,8 +1,20 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS info.picocli:picocli:4.6.3
 //DEPS ch.qos.reload4j:reload4j:1.2.24
+//DEPS com.opencsv:opencsv:5.7.1
 
+import com.opencsv.*;
+import com.opencsv.exceptions.CsvException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
+import java.util.stream.Stream;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import picocli.CommandLine;
@@ -24,8 +36,43 @@ class DateUpdater implements Callable<Integer> {
     }
 
     @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        logger.info("Hello " + greeting);
+    public Integer call() throws Exception {
+        try (Stream<Path> stream = Files.walk(Paths.get("."), 1)) {
+            stream
+                .filter(file -> !Files.isDirectory(file) && file.getFileName().toString().endsWith("csv"))
+                .forEach(path -> {
+                    try (Reader reader = Files.newBufferedReader(path)) {
+                        CSVParser parser = new CSVParserBuilder().withSeparator(';').withIgnoreQuotations(true).build();
+                        CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(0).withCSVParser(parser).build();
+                        var lines = csvReader.readAll();
+                        if (!lines.isEmpty() && Arrays.toString(lines.get(0)).contains("created_at")) {
+                            lines
+                                .subList(1, lines.size())
+                                .forEach(line -> {
+                                    LocalDateTime today = LocalDateTime.now();
+                                    LocalDateTime date = LocalDateTime.parse(line[line.length - 1]);
+                                    LocalDateTime adjustedDate = date.withYear(today.getYear()).withMonth(today.getMonthValue());
+                                    line[line.length - 1] = adjustedDate.toString();
+                                });
+                        }
+                        try (
+                            CSVWriter writer = new CSVWriter(
+                                new FileWriter(path.toString()),
+                                ';',
+                                CSVWriter.NO_QUOTE_CHARACTER,
+                                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                                CSVWriter.DEFAULT_LINE_END
+                            )
+                        ) {
+                            lines.forEach(writer::writeNext);
+                        }
+                    } catch (IOException | CsvException ex) {
+                        logger.error(ex);
+                    }
+                });
+        } catch (IOException ex) {
+            logger.error(ex);
+        }
         return 0;
     }
 }
