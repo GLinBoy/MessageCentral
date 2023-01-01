c15338614b329b50ad98846ca926310f00fcfed0
GLinBoy <hojjatabedie@gmail.com>
2023-01-01 12:30:36 +0100

    👌 IMPROVE: Replace the title of submit button on the new Email page with send
    resolved #716

--- src/main/webapp/app/entities/email/email-multiple.vue
+++ src/main/webapp/app/entities/email/email-multiple.vue
@@ -98,7 +98,7 @@
             :disabled="$v.emails.$invalid || isSaving"
             class="btn btn-primary"
           >
-            <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.save')">Save</span>
+            <font-awesome-icon icon="save"></font-awesome-icon>&nbsp;<span v-text="$t('entity.action.send')">Save</span>
           </button>
         </div>
       </form>
