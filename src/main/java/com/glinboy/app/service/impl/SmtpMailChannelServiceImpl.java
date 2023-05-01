2708dfa5e5651812c0c8015b58b84cff88c24c2f
GLinBoy <hojjatabedie@gmail.com>
2023-05-01 14:37:44 +0200

    👌 IMPROVE: Applied Mail type during send process
    resolved #732

--- src/main/java/com/glinboy/app/service/impl/SmtpMailChannelServiceImpl.java
+++ src/main/java/com/glinboy/app/service/impl/SmtpMailChannelServiceImpl.java
@@ -1,14 +1,16 @@
 package com.glinboy.app.service.impl;
 
 import com.glinboy.app.config.ApplicationProperties;
+import com.glinboy.app.domain.enumeration.EmailType;
 import com.glinboy.app.service.dto.EmailDTO;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import javax.mail.internet.MimeMessage;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 @ConditionalOnProperty(value = "application.email.provider", havingValue = "mail-server", matchIfMissing = true)
@@ -16,27 +18,29 @@ public class SmtpMailChannelServiceImpl extends AbstractMailChannelServiceImpl {
 
     private final JavaMailSender emailSender;
 
-    protected SmtpMailChannelServiceImpl(ApplicationEventPublisher publisher,
-                                         ApplicationProperties properties,
-                                         JavaMailSender emailSender) {
+    protected SmtpMailChannelServiceImpl(
+        ApplicationEventPublisher publisher,
+        ApplicationProperties properties,
+        JavaMailSender emailSender
+    ) {
         super(publisher, properties);
         this.emailSender = emailSender;
     }
 
     @Override
     public void deliverMessage(EmailDTO... emailsDTO) throws Exception {
-        var messages = new SimpleMailMessage[emailsDTO.length];
+        var messages = new MimeMessage[emailsDTO.length];
         for (var i = 0; i < emailsDTO.length; i++) {
             var emailDTO = emailsDTO[i];
-            var message = new SimpleMailMessage();
-            message.setFrom(properties.getEmail().getFrom());
-            message.setTo(emailDTO.getReceiver());
-            message.setSubject(emailDTO.getSubject());
-            message.setText(emailDTO.getContent());
+            var message = emailSender.createMimeMessage();
+            var helper = new MimeMessageHelper(message, StandardCharsets.UTF_8.name());
+            helper.setFrom(properties.getEmail().getFrom());
+            helper.setTo(emailDTO.getReceiver());
+            helper.setSubject(emailDTO.getSubject());
+            helper.setText(emailDTO.getContent(), EmailType.HTML.equals(emailDTO.getEmailType()));
             messages[i] = message;
         }
         emailSender.send(messages);
         log.info("Mail(s) sent! {}", Arrays.toString(emailsDTO));
     }
-
 }
