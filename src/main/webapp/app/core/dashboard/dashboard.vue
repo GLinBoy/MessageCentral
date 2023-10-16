9009b929d4b661308e5928300a5a2ee2ba7719ad
GLinBoy <hojjatabedie@gmail.com>
2023-10-16 20:46:52 +0200

    👌 IMPROVE: Reordered cards in dashboard

--- src/main/webapp/app/core/dashboard/dashboard.vue
+++ src/main/webapp/app/core/dashboard/dashboard.vue
@@ -22,7 +22,7 @@
             <b-card-text>
               <b-row class="text-center">
                 <b-col lg="4">
-                  <font-awesome-icon class="fa-3x" icon="comments" />
+                  <font-awesome-icon class="fa-3x" icon="comments" color="primary" />
                 </b-col>
                 <b-col lg="8">
                   <span class="font-weight-bolder text-uppercase">Sent: {{ totalSuccessfulSent }}</span>
@@ -61,31 +61,6 @@
         <b-col sm="12" md="3">
           <b-card
             border-variant="primary"
-            :header="$t('dashboard.cards.cardsTitle.notifications')"
-            header-bg-variant="primary"
-            header-text-variant="white"
-            align="center"
-            class="mb-2"
-          >
-            <b-card-text>
-              <b-row class="text-center">
-                <b-col lg="4">
-                  <router-link to="/notification">
-                    <font-awesome-icon class="fa-3x" icon="bell" />
-                  </router-link>
-                </b-col>
-                <b-col lg="8">
-                  <span class="font-weight-bolder text-uppercase">Sent: {{ notificationsSuccessfulSent }}</span>
-                  <br />
-                  <span class="font-weight-lighter">Failed: {{ notificationsFailedSent }}</span>
-                </b-col>
-              </b-row>
-            </b-card-text>
-          </b-card>
-        </b-col>
-        <b-col sm="12" md="3">
-          <b-card
-            border-variant="primary"
             :header="$t('dashboard.cards.cardsTitle.shortMessages')"
             header-bg-variant="primary"
             header-text-variant="white"
@@ -103,6 +78,31 @@
                   <span class="font-weight-bolder text-uppercase">Sent: {{ smsSuccessfulSent }}</span>
                   <br />
                   <span class="font-weight-lighter">Failed: {{ smsFailedSent }}</span>
+                </b-col>
+              </b-row>
+            </b-card-text>
+          </b-card>
+        </b-col>
+        <b-col sm="12" md="3">
+          <b-card
+            border-variant="primary"
+            :header="$t('dashboard.cards.cardsTitle.notifications')"
+            header-bg-variant="primary"
+            header-text-variant="white"
+            align="center"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b-row class="text-center">
+                <b-col lg="4">
+                  <router-link to="/notification">
+                    <font-awesome-icon class="fa-3x" icon="bell" />
+                  </router-link>
+                </b-col>
+                <b-col lg="8">
+                  <span class="font-weight-bolder text-uppercase">Sent: {{ notificationsSuccessfulSent }}</span>
+                  <br />
+                  <span class="font-weight-lighter">Failed: {{ notificationsFailedSent }}</span>
                 </b-col>
               </b-row>
             </b-card-text>
